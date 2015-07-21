828fad1974c99dcebe74b43fa6d63560ca464be9
Jonas Gerdes <jonas.gerdes@gmx.net>
2015-07-21 19:46:39 +0200

    test: limit view to area of stoppelmarkt

--- app/src/main/java/com/jonasgerdes/stoppelmap/MainActivity.java
+++ app/src/main/java/com/jonasgerdes/stoppelmap/MainActivity.java
@@ -3,15 +3,26 @@ package com.jonasgerdes.stoppelmap;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    public static final double LAT_MAX = 52.75067702042563;
+    public static final double LONG_MIN = 8.291684053838253;
+    public static final double LAT_MIN = 52.744443959019534;
+    public static final double LOT_MAX = 8.301458023488522;
+    public static final float ZOOM_MAX = 15;
+    public static final float ZOOM_MIN = 18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,30 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap map) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(52.747995, 8.295607), 16));
+        map.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
 
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                Log.d("camera", "lon:" + cameraPosition.target.longitude + "; lat:" + cameraPosition.target.latitude+"; z:"+cameraPosition.zoom);
+                LatLng newPos = new LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude);
+                float zoom = cameraPosition.zoom;
+                if (cameraPosition.target.latitude > LAT_MAX){
+                    newPos = new LatLng(LAT_MAX, newPos.longitude);
+                }
+                if (cameraPosition.target.longitude < LONG_MIN){
+                    newPos = new LatLng(newPos.latitude, LONG_MIN);
+                }
+                if (cameraPosition.target.latitude < LAT_MIN){
+                    newPos = new LatLng(LAT_MIN, newPos.longitude);
+                }
+                if (cameraPosition.target.longitude > LOT_MAX){
+                    newPos = new LatLng(newPos.latitude, LOT_MAX);
+                }
+                zoom = Math.max(Math.min(zoom, ZOOM_MIN), ZOOM_MAX);
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(newPos, zoom));
+            }
+        });
     }
 }
