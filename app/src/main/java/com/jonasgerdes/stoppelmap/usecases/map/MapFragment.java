21910dcfdab510b5f6ae026774ea6653ca62f6fe
Jonas Gerdes <hello@jonasgerdes.com>
2016-07-03 16:29:55 +0200

    Add GoogleMaps

--- app/src/main/java/com/jonasgerdes/stoppelmap/usecases/map/MapFragment.java
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecases/map/MapFragment.java
@@ -1,19 +1,31 @@
 package com.jonasgerdes.stoppelmap.usecases.map;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.jonasgerdes.stoppelmap.R;
 
 /**
  * Created by Jonas on 03.07.2016.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Nullable
     @Override
@@ -22,12 +34,50 @@ public class MapFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                FragmentManager fm = getChildFragmentManager();
+                SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+                fm.beginTransaction()
+                        .replace(R.id.map_placeholder, mapFragment).commit();
+                mapFragment.getMapAsync(MapFragment.this);
+            }
+        }, 400);
+
+    }
+
     public static MapFragment newInstance() {
-        
+
         Bundle args = new Bundle();
-        
+
         MapFragment fragment = new MapFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: 03.07.2016
+        } else {
+            mMap.setMyLocationEnabled(true);
+        }
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setTiltGesturesEnabled(false);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.getUiSettings().setZoomControlsEnabled(false);
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(52.747995, 8.295607), 16));
+
     }
 }
