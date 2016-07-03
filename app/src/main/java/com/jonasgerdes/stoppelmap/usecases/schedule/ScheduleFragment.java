0af6ba4b78cf54b74057425782a93fd7f9ab57ac
Jonas Gerdes <hello@jonasgerdes.com>
2016-07-03 21:09:00 +0200

    Create schedule fragment stub

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecases/schedule/ScheduleFragment.java
@@ -0,0 +1,32 @@
+package com.jonasgerdes.stoppelmap.usecases.schedule;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jonasgerdes.stoppelmap.R;
+
+/**
+ * Created by Jonas on 03.07.2016.
+ */
+public class ScheduleFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_schedule, container, false);
+        return view;
+    }
+
+    public static ScheduleFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        ScheduleFragment fragment = new ScheduleFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
