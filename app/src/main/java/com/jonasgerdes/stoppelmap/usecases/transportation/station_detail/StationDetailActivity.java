feac4823f7b8541d7865b1d1b3e3f097c9665d25
Jonas Gerdes <hello@jonasgerdes.com>
2016-08-03 19:18:09 +0200

    Add options to navigate to and show price of stations

--- app/src/main/java/com/jonasgerdes/stoppelmap/usecases/transportation/station_detail/StationDetailActivity.java
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecases/transportation/station_detail/StationDetailActivity.java
@@ -2,21 +2,32 @@ package com.jonasgerdes.stoppelmap.usecases.transportation.station_detail;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 import com.jonasgerdes.stoppelmap.R;
 import com.jonasgerdes.stoppelmap.StoppelMapApp;
+import com.jonasgerdes.stoppelmap.model.transportation.Price;
 import com.jonasgerdes.stoppelmap.model.transportation.Station;
+
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class StationDetailActivity extends AppCompatActivity {
+    private static final String TAG = "StationDetailActivity";
 
     private static final String EXTRA_STATION_ID = "EXTRA_STATION_ID";
     private static final String EXTRA_STATION_NAME = "EXTRA_STATION_NAME";
@@ -28,6 +39,7 @@ public class StationDetailActivity extends AppCompatActivity {
     ViewPager mViewPager;
 
     private DepartureDayFragmentAdapter mDayPageAdapter;
+    private Station mStation;
 
 
     @Override
@@ -51,12 +63,18 @@ public class StationDetailActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
 
-        Station station = StoppelMapApp.getViaActivity(this)
+        mStation = StoppelMapApp.getViaActivity(this)
                 .getRealm().where(Station.class).equalTo("uuid", stationUuid).findFirst();
 
-        mDayPageAdapter = new DepartureDayFragmentAdapter(this, getSupportFragmentManager(), station);
+        mDayPageAdapter = new DepartureDayFragmentAdapter(this, getSupportFragmentManager(), mStation);
         mViewPager.setAdapter(mDayPageAdapter);
         mTabs.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.station_detail, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
 
@@ -66,9 +84,63 @@ public class StationDetailActivity extends AppCompatActivity {
             case android.R.id.home:
                 onBackPressed();
                 return true;
+            case R.id.price:
+                showPriceDialog();
+                return true;
+            case R.id.directions:
+                openDirections();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void openDirections() {
+        double lat = mStation.getGeoLocation().getLat();
+        double lng = mStation.getGeoLocation().getLng();
+        String schema = "google.navigation:q=%f,%f&mode=w";
+        String uriString = String.format(Locale.US, schema, lat, lng);
+        Log.d(TAG, "openDirections: " + uriString);
+        Uri gmmIntentUri = Uri.parse(uriString);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        } else {
+            schema = "https://www.google.com/maps/preview?saddr=My+Location&daddr=%f,%f&dirflg=w";
+            uriString = String.format(Locale.US, schema, lat, lng);
+            Log.d(TAG, "openDirections: " + uriString);
+            gmmIntentUri = Uri.parse(uriString);
+            mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            startActivity(mapIntent);
+        }
+
+    }
+
+    private void showPriceDialog() {
+        GridLayout priceGrid = (GridLayout) LayoutInflater.from(this)
+                .inflate(R.layout.transportation_sation_detail_price, null);
+
+        String[] prices = new String[4];
+
+        for (int i = 0; i < prices.length / 2; i++) {
+            if (mStation.getPrices().size() >= i) {
+                Price price = mStation.getPrices().get(i);
+                prices[i * 2] = price.getType();
+                prices[i * 2 + 1] = String.format("%.2f€", price.getPrice() / 100f);
+            }
+        }
+
+        for (int i = 0; i < prices.length && i < priceGrid.getChildCount(); i++) {
+            ((TextView) priceGrid.getChildAt(i)).setText(prices[i]);
+        }
+
+
+        new AlertDialog.Builder(this, R.style.StoppelMapAlert)
+                .setTitle("Preise ab " + mStation.getName())
+                .setView(priceGrid)
+                .setPositiveButton("Ok", null)
+                .show();
     }
 
 
