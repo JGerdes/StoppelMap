abf4a06beddf9f15bfe25eb8215599085d2b48f8
Jonas Gerdes <dev@jonasgerdes.com>
2017-02-25 13:56:40 +0100

    Hide colors from wallpaper if to dark

--- app/src/main/java/com/jonasgerdes/stoppelmap/widget/options/ColorOptionPage.java
+++ app/src/main/java/com/jonasgerdes/stoppelmap/widget/options/ColorOptionPage.java
@@ -3,6 +3,7 @@ package com.jonasgerdes.stoppelmap.widget.options;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.graphics.ColorUtils;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
@@ -10,10 +11,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.jonasgerdes.stoppelmap.R;
+import com.jonasgerdes.stoppelmap.util.ViewUtil;
 import com.jonasgerdes.stoppelmap.views.colorpicker.ColorPicker;
 import com.jonasgerdes.stoppelmap.widget.ColorableWidgetPreview;
 
+import java.util.List;
+
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 
 /**
@@ -23,6 +28,8 @@ import butterknife.ButterKnife;
 public class ColorOptionPage extends OptionPage<ColorableWidgetPreview>
         implements View.OnClickListener {
 
+    private static final float MIN_LIGHTNESS = 0.2f;
+    private static final float MAX_LIGHTNESS = 0.8f;
     @BindView(R.id.widget_color_selection_list)
     ViewGroup mColorList;
 
@@ -38,8 +45,12 @@ public class ColorOptionPage extends OptionPage<ColorableWidgetPreview>
     @BindView(R.id.color_picker)
     ColorPicker mColorPicker;
 
+    @BindViews({ R.id.from_wallpaper_list, R.id.from_wallpaper_text})
+    List<View> mFromWallpaper;
+
     private int mDefaultColor;
     private Palette mPalette;
+
 
 
     @Nullable
@@ -78,48 +89,71 @@ public class ColorOptionPage extends OptionPage<ColorableWidgetPreview>
                 || mColorSelectionMuted == null) {
             return;
         }
-        mColorSelectionDominant
-                .setCardBackgroundColor(mPalette.getDominantColor(Color.BLACK));
-        mColorSelectionVibrant
-                .setCardBackgroundColor(mPalette.getVibrantColor(Color.BLACK));
-        mColorSelectionMuted
-                .setCardBackgroundColor(mPalette.getMutedColor(Color.BLACK));
 
-        mColorSelectionDominant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int color = mPalette.getDominantColor(Color.BLACK);
-                getEditableWidgetPreview().setColorsBy(color);
-                mColorPicker.setColor(color);
-                getWidgetPreview().update();
-            }
-        });
-        mColorSelectionVibrant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int[] colors = {
-                        mPalette.getLightVibrantColor(Color.BLACK),
-                        mPalette.getVibrantColor(Color.BLACK),
-                        mPalette.getDarkVibrantColor(Color.BLACK)
-                };
-                mColorPicker.setColor(colors[1]);
-                getEditableWidgetPreview().setColors(colors);
-                getWidgetPreview().update();
-            }
-        });
-        mColorSelectionMuted.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int[] colors = {
-                        mPalette.getLightMutedColor(Color.BLACK),
-                        mPalette.getMutedColor(Color.BLACK),
-                        mPalette.getDarkMutedColor(Color.BLACK)
-                };
-                mColorPicker.setColor(colors[1]);
-                getEditableWidgetPreview().setColors(colors);
-                getWidgetPreview().update();
-            }
-        });
+        boolean atLeastOneWallpaperColorUsable = false;
+
+        final int dominantColor = mPalette.getDominantColor(Color.BLACK);
+        if (isLightnessIsBetween(dominantColor, MIN_LIGHTNESS, MAX_LIGHTNESS)) {
+            mColorSelectionDominant.setCardBackgroundColor(dominantColor);
+            mColorSelectionDominant.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int color = dominantColor;
+                    getEditableWidgetPreview().setColorsBy(color);
+                    mColorPicker.setColor(color);
+                    getWidgetPreview().update();
+                }
+            });
+            atLeastOneWallpaperColorUsable = true;
+        } else {
+            mColorSelectionDominant.setVisibility(View.GONE);
+        }
+
+        final int vibrantColor = mPalette.getVibrantColor(Color.BLACK);
+        if (isLightnessIsBetween(vibrantColor, MIN_LIGHTNESS, MAX_LIGHTNESS)) {
+            mColorSelectionVibrant.setCardBackgroundColor(vibrantColor);
+            mColorSelectionVibrant.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int[] colors = {
+                            mPalette.getLightVibrantColor(vibrantColor),
+                            vibrantColor,
+                            mPalette.getDarkVibrantColor(vibrantColor)
+                    };
+                    mColorPicker.setColor(vibrantColor);
+                    getEditableWidgetPreview().setColors(colors);
+                    getWidgetPreview().update();
+                }
+            });
+            atLeastOneWallpaperColorUsable = true;
+        } else {
+            mColorSelectionVibrant.setVisibility(View.GONE);
+        }
+
+        final int mutedColor = mPalette.getMutedColor(Color.BLACK);
+        if (isLightnessIsBetween(mutedColor, MIN_LIGHTNESS, MAX_LIGHTNESS)) {
+            mColorSelectionMuted.setCardBackgroundColor(mutedColor);
+            mColorSelectionMuted.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int[] colors = {
+                            mPalette.getLightMutedColor(mutedColor),
+                            mutedColor,
+                            mPalette.getDarkMutedColor(mutedColor)
+                    };
+                    mColorPicker.setColor(mutedColor);
+                    getEditableWidgetPreview().setColors(colors);
+                    getWidgetPreview().update();
+                }
+            });
+            atLeastOneWallpaperColorUsable = true;
+        } else {
+            mColorSelectionMuted.setVisibility(View.GONE);
+        }
+
+        if (!atLeastOneWallpaperColorUsable) {
+            ButterKnife.apply(mFromWallpaper, ViewUtil.HIDE);
+        }
     }
 
 
@@ -144,6 +178,12 @@ public class ColorOptionPage extends OptionPage<ColorableWidgetPreview>
         mPalette = palette;
         setUpPalette();
         return this;
+    }
 
+
+    private static boolean isLightnessIsBetween(int color, float lowerBound, float upperBound) {
+        float[] hsl = new float[3];
+        ColorUtils.colorToHSL(color, hsl);
+        return hsl[2] >= lowerBound && hsl[2] <= upperBound;
     }
 }
