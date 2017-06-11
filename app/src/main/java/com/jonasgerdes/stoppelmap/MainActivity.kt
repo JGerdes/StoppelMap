a3abe4dcbe6649d2bfd031b23f1f06372350fb4b
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-11 21:38:23 +0200

    Change icon behavior in bottom navigation

--- app/src/main/java/com/jonasgerdes/stoppelmap/MainActivity.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/MainActivity.kt
@@ -2,12 +2,17 @@ package com.jonasgerdes.stoppelmap
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jonasgerdes.stoppelmap.util.enableItemShifting
+import com.jonasgerdes.stoppelmap.util.enableItemTextHiding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigation.enableItemShifting(false)
+        navigation.enableItemTextHiding(true)
     }
 
 }
