0feb1f22e9ddc9fac4d7b53254f50a7fddd97977
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-13 21:39:21 +0200

    Create flavor structure

---
+++ app/src/admin/java/com/jonasgerdes/stoppelmap/admin/AdminActivity.kt
@@ -0,0 +1,17 @@
+package com.jonasgerdes.stoppelmap.admin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jonasgerdes.stoppelmap.R
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 11.06.2017
+ */
+class AdminActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin)
+    }
+}
---
+++ app/src/store/java/com/jonasgerdes/stoppelmap/admin/Administration.kt
@@ -0,0 +1,14 @@
+package com.jonasgerdes.stoppelmap.admin
+
+import android.content.Context
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 11.06.2017
+ */
+object Administration {
+
+    fun init(context: Context) {
+        //no-op for store builds
+    }
+}
