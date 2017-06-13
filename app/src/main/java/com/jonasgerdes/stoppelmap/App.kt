c128b802220b2f2b6816adecb93d344ff9842c33
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-13 22:41:38 +0200

    Use Realm and RealmBrowser

--- app/src/main/java/com/jonasgerdes/stoppelmap/App.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/App.kt
@@ -2,6 +2,9 @@ package com.jonasgerdes.stoppelmap
 
 import android.app.Application
 import com.jonasgerdes.stoppelmap.admin.Administration
+import de.jonasrottmann.realmbrowser.RealmBrowser
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
@@ -11,6 +14,13 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Realm.init(this)
+        Realm.setDefaultConfiguration(RealmConfiguration.Builder()
+                .deleteRealmIfMigrationNeeded()
+                .build())
+
         Administration.init(this)
+        RealmBrowser.addFilesShortcut(this)
     }
 }
