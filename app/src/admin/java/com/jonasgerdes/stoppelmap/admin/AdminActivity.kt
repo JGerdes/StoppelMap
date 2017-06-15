bd935a086bc57f5d574c37112360a564c68cadb4
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-15 12:51:36 +0200

    Read rides.json from assets and save data Realm

--- app/src/admin/java/com/jonasgerdes/stoppelmap/admin/AdminActivity.kt
+++ app/src/admin/java/com/jonasgerdes/stoppelmap/admin/AdminActivity.kt
@@ -2,7 +2,11 @@ package com.jonasgerdes.stoppelmap.admin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.gson.GsonBuilder
 import com.jonasgerdes.stoppelmap.R
+import com.jonasgerdes.stoppelmap.model.entity.map.MapEntity
+import com.jonasgerdes.stoppelmap.util.readAsString
+import io.realm.Realm
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
@@ -10,8 +14,31 @@ import com.jonasgerdes.stoppelmap.R
  */
 class AdminActivity : AppCompatActivity() {
 
+    companion object {
+        val PATH_ENTITIES = "json/entities/"
+    }
+
+    val gson = GsonBuilder().create()
+    lateinit var realm: Realm
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+        realm = Realm.getDefaultInstance()
+        loadData()
+    }
+
+    fun loadData() {
+        val json = assets.readAsString(PATH_ENTITIES + "rides.json")
+        val rides = gson.fromJson(json, MapEntity.MapEntities::class.java)
+        realm.executeTransaction {
+            realm.deleteAll()
+            realm.copyToRealm(rides)
+        }
+    }
+
+    override fun onDestroy() {
+        realm.close()
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/model/entity/GeoLocation.kt
@@ -0,0 +1,15 @@
+package com.jonasgerdes.stoppelmap.model.entity
+
+import com.google.gson.annotations.SerializedName
+import io.realm.RealmObject
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 13.06.2017
+ */
+open class GeoLocation : RealmObject() {
+    @SerializedName("lon")
+    var longitude: Double = 0.0
+    @SerializedName("lat")
+    var latitude: Double = 0.0
+}
