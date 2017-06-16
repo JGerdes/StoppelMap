86db062604d7cf37c2a572a5ad87c96d088df394
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-16 18:34:17 +0200

    Set restriction to map camera

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/settings.kt
@@ -0,0 +1,18 @@
+package com.jonasgerdes.stoppelmap
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 16.06.2017
+ */
+
+object Settings {
+    val cameraBounds = LatLngBounds(
+            LatLng(52.7429499584193, 8.28653654801576),
+            LatLng(52.7494011815008, 8.30059127365977)
+    )
+    val maxZoom = 20f
+    val minZoom = 15f
+}
