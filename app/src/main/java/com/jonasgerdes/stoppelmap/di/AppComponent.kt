010e2f9bd748980f72a4c8ff788db9aeb623d03a
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-16 15:02:35 +0200

    Use Dagger2 for dependeny injection

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/di/AppComponent.kt
@@ -0,0 +1,14 @@
+package com.jonasgerdes.stoppelmap.di
+
+import com.jonasgerdes.stoppelmap.di.module.AppModule
+import dagger.Component
+import javax.inject.Singleton
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 16.06.2017
+ */
+@Singleton
+@Component(modules = arrayOf(AppModule::class))
+interface AppComponent {
+}
