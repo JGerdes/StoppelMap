7a5aba9befc4705d8730a67870f518f98ebde609
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-18 15:01:49 +0200

    Include dependencies for RxJava and RxAndroid

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecase/map/viewmodel/MapInteractor.kt
@@ -0,0 +1,11 @@
+package com.jonasgerdes.stoppelmap.usecase.map.viewmodel
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 18.06.2017
+ */
+class MapViewModel : ViewModel() {
+
+}
