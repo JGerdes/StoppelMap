7344ada7c5f37ce458b794c1dc87290aeafea5b1
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-22 18:16:44 +0200

    Add placeholder fragments for other usecases

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecase/information/view/InformationFragment.kt
@@ -0,0 +1,9 @@
+package com.jonasgerdes.stoppelmap.usecase.information.view
+
+import android.arch.lifecycle.LifecycleFragment
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 22.06.2017
+ */
+class InformationFragment : LifecycleFragment()
