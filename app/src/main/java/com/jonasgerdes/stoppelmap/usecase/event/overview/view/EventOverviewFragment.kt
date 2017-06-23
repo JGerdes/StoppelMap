97fb5da6f6e2a5ed78a3b716509984d8922b496b
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-23 12:30:16 +0200

    Add placeholder layouts for usescases

--- app/src/main/java/com/jonasgerdes/stoppelmap/usecase/event/overview/view/EventOverviewFragment.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecase/event/overview/view/EventOverviewFragment.kt
@@ -1,10 +1,19 @@
 package com.jonasgerdes.stoppelmap.usecase.event.overview.view
 
 import android.arch.lifecycle.LifecycleFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jonasgerdes.stoppelmap.R
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
  * @since 22.06.2017
  */
 class EventOverviewFragment : LifecycleFragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.event_overview_fragment, container, false)
+    }
 }
