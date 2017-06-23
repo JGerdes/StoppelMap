fee34917022cf49987b737c042ba8d6dbdbc873d
Jonas Gerdes <dev@jonasgerdes.com>
2017-06-23 12:30:16 +0200

    Add placeholder layouts for usescases

--- app/src/main/java/com/jonasgerdes/stoppelmap/usecase/information/view/InformationFragment.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecase/information/view/InformationFragment.kt
@@ -1,9 +1,19 @@
 package com.jonasgerdes.stoppelmap.usecase.information.view
 
 import android.arch.lifecycle.LifecycleFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jonasgerdes.stoppelmap.R
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
  * @since 22.06.2017
  */
-class InformationFragment : LifecycleFragment()
+class InformationFragment : LifecycleFragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.information_fragment, container, false)
+    }
+}
--- app/src/main/java/com/jonasgerdes/stoppelmap/usecase/transportation/overview/view/TransportOverviewFragment.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecase/transportation/overview/view/TransportOverviewFragment.kt
@@ -1,9 +1,19 @@
 package com.jonasgerdes.stoppelmap.usecase.transportation.overview.view
 
 import android.arch.lifecycle.LifecycleFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jonasgerdes.stoppelmap.R
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
  * @since 22.06.2017
  */
-class TransportOverviewFragment : LifecycleFragment()
+class TransportOverviewFragment : LifecycleFragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.transportation_overview_fragment, container, false)
+    }
+}
