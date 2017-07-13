3e5d9ed3c2c511911f275f6e12a68ef22670ae3b
Jonas Gerdes <dev@jonasgerdes.com>
2017-07-13 11:17:14 +0200

    Use Anko for view creation in code

--- app/src/main/java/com/jonasgerdes/stoppelmap/usecase/map/view/search/SearchResultHolder.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecase/map/view/search/SearchResultHolder.kt
@@ -1,16 +1,16 @@
 package com.jonasgerdes.stoppelmap.usecase.map.view.search
 
-import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.View
-import android.widget.ImageView
+import android.widget.LinearLayout
 import com.jonasgerdes.stoppelmap.R
 import com.jonasgerdes.stoppelmap.model.entity.map.search.MapSearchResult
 import com.jonasgerdes.stoppelmap.model.entity.map.search.SingleEntitySearchResult
 import com.jonasgerdes.stoppelmap.util.Assets
-import com.jonasgerdes.stoppelmap.util.getDrawableCompat
 import com.jonasgerdes.stoppelmap.util.setTint
 import kotlinx.android.synthetic.main.map_search_result_single.view.*
+import org.jetbrains.anko.dip
+import org.jetbrains.anko.imageView
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
@@ -31,27 +31,23 @@ abstract class SearchResultHolder<in E : MapSearchResult>(itemView: View)
             } else {
                 itemView.alias.visibility = View.GONE
             }
-            setIcons(listOf(Assets.getIconFor(result.entity))
-                    .filter { i -> i != Assets.NONE }
-                    .map {
-                        itemView.context.getDrawableCompat(it)
-                    })
+            setIcons(Assets.getIconsFor(result.entity)
+                    .filter { i -> i != Assets.NONE })
         }
 
-        fun setIcons(icons: List<Drawable>) {
+        fun setIcons(icons: List<Int>) {
             itemView.iconList.removeAllViews()
-            icons.map {
-                val view = ImageView(itemView.context)
-                view.setImageDrawable(it)
-                view.setTint(R.color.tint_icon_active)
-                view
-            }.forEach {
-                itemView.iconList.addView(it)
-            }
-            itemView.iconList.visibility = if (icons.isEmpty()) {
-                View.GONE
-            } else {
-                View.VISIBLE
+            with(itemView.iconList) {
+                icons.forEach {
+                    imageView(it) {
+                        setTint(R.color.tint_icon_active)
+                    }.layoutParams = LinearLayout.LayoutParams(dip(16), dip(16))
+                }
+                visibility = if (icons.isEmpty()) {
+                    View.GONE
+                } else {
+                    View.VISIBLE
+                }
             }
         }
 
