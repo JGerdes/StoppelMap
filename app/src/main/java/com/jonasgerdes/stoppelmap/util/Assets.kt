06bfc2110dc842e467e5623994dee164dc9ea7a1
Jonas Gerdes <dev@jonasgerdes.com>
2017-07-13 11:17:14 +0200

    Use Anko for view creation in code

--- app/src/main/java/com/jonasgerdes/stoppelmap/util/Assets.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/util/Assets.kt
@@ -9,9 +9,9 @@ import com.jonasgerdes.stoppelmap.model.entity.map.*
  */
 object Assets {
 
-    val NONE = -1;
+    val NONE = -1
 
-    fun getIconFor(entity: MapEntity): Int {
+    fun getTypeIconFor(entity: MapEntity): Int {
         return when (entity.type) {
             Bar.TYPE -> R.drawable.ic_entity_bar_black_16dp
             CandyStall.TYPE -> NONE
@@ -25,4 +25,46 @@ object Assets {
             else -> NONE
         }
     }
+
+    fun getIconsFor(entity: MapEntity): List<Int> {
+        val icons = ArrayList<Int>()
+        if (getTypeIconFor(entity) != NONE) {
+            icons.add(getTypeIconFor(entity))
+        }
+
+        icons.addAll(when (entity.type) {
+            Bar.TYPE -> getIconsFor(entity.bar!!)
+            CandyStall.TYPE -> emptyList<Int>()
+            Exhibition.TYPE -> emptyList<Int>()
+            FoodStall.TYPE -> getIconsFor(entity.foodStall!!)
+            GameStall.TYPE -> emptyList<Int>()
+            Restroom.TYPE -> emptyList<Int>()
+            Ride.TYPE -> emptyList<Int>()
+            SellerStall.TYPE -> emptyList<Int>()
+            else -> emptyList<Int>()
+        });
+
+        return icons
+    }
+
+    private fun getIconsFor(foodStall: FoodStall): List<Int> {
+        val icons = ArrayList<Int>()
+        if (foodStall.isTent) {
+            icons.add(R.drawable.ic_entity_tent_black_16dp)
+        }
+        if (foodStall.isBar) {
+            icons.add(R.drawable.ic_entity_bar_black_16dp)
+        }
+        return icons
+    }
+
+    private fun getIconsFor(bar: Bar): List<Int> {
+        val icons = ArrayList<Int>()
+        if (bar.isTent) {
+            icons.add(R.drawable.ic_entity_tent_black_16dp)
+        }
+        return icons
+    }
+
+
 }
