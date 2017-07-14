276cd7d2d5e444cacceb361d1da332e8427a0d3e
Jonas Gerdes <dev@jonasgerdes.com>
2017-07-14 10:32:09 +0200

    Fix memory leak of MainPresenter and state related issue

--- app/src/main/java/com/jonasgerdes/stoppelmap/usecase/main/presenter/MainPresenter.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecase/main/presenter/MainPresenter.kt
@@ -2,28 +2,41 @@ package com.jonasgerdes.stoppelmap.usecase.main.presenter
 
 import com.jonasgerdes.stoppelmap.usecase.main.viewmodel.MainInteractor
 import com.jonasgerdes.stoppelmap.usecase.main.viewmodel.MainViewState
+import com.jonasgerdes.stoppelmap.util.plusAssign
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import java.util.concurrent.TimeUnit
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
  * @since 22.06.2017
  */
-class MainPresenter(private val view: MainView, interactor: MainInteractor) {
+class MainPresenter(private val view: MainView, interactor: MainInteractor) : Disposable {
+    val disposable = CompositeDisposable()
+
     init {
-        interactor.state
+        disposable += interactor.state
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this::render)
 
-        view.getNavigationEvents()
+        disposable += view.getNavigationEvents()
                 .map { it.itemId }
-                .debounce(100, TimeUnit.MILLISECONDS)
                 .distinctUntilChanged()
+                .debounce(100, TimeUnit.MILLISECONDS)
                 .subscribe(interactor::onNavigationClicked)
     }
 
     private fun render(state: MainViewState) {
         view.showView(state)
         view.selectNavigation(state.selectedItemId)
+    }
+
+    override fun isDisposed(): Boolean {
+        return disposable.isDisposed
+    }
+
+    override fun dispose() {
+        disposable.dispose()
     }
 }
