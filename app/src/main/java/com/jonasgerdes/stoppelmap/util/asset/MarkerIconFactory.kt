24797045100cd87d2255d033c142e34ae20a45d9
Jonas Gerdes <dev@jonasgerdes.com>
2017-07-20 13:36:38 +0200

    Draw marker with icons

--- app/src/main/java/com/jonasgerdes/stoppelmap/util/asset/MarkerIconFactory.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/util/asset/MarkerIconFactory.kt
@@ -2,21 +2,25 @@ package com.jonasgerdes.stoppelmap.util.asset
 
 import android.content.Context
 import android.graphics.*
+import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.jonasgerdes.stoppelmap.R
+import com.jonasgerdes.stoppelmap.util.asset.Assets.NONE
 
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
  * @since 20.06.17
  */
-class MarkerIconFactory(context: Context) {
+class MarkerIconFactory(val context: Context) {
 
-    val textFillPaint: Paint = Paint()
-    val textOutlinePaint: Paint
-    val padding: Int
+    private val textFillPaint: Paint = Paint()
+    private val textOutlinePaint: Paint
+    private val padding: Int
+    private val iconSize: Int
+    private val iconColor: Int
 
     init {
         val resources = context.resources
@@ -32,7 +36,6 @@ class MarkerIconFactory(context: Context) {
         textOutlinePaint = Paint(textFillPaint)
         with(textOutlinePaint) {
             color = ContextCompat.getColor(context, R.color.text_light)
-            alpha = 192
             style = Paint.Style.STROKE
             strokeWidth = resources.getDimensionPixelSize(R.dimen.map_marker_stroke_width).toFloat()
             strokeCap = Paint.Cap.ROUND
@@ -40,30 +43,45 @@ class MarkerIconFactory(context: Context) {
         }
 
         padding = resources.getDimensionPixelSize(R.dimen.map_marker_padding)
+        iconSize = context.resources.getDimensionPixelSize(R.dimen.map_marker_icon_size)
+        iconColor = ContextCompat.getColor(context, R.color.map_marker_icon)
 
     }
 
     fun createMarker(title: String, iconResource: Int): BitmapDescriptor {
+        var icon: Drawable? = null
+        if (iconResource != NONE) {
+            icon = ContextCompat.getDrawable(context, iconResource)
+        }
+
         val textBounds = Rect()
         textOutlinePaint.getTextBounds(title, 0, title.length, textBounds)
 
+        var markerWidth = textBounds.width() + padding * 2
+        var textX = padding.toFloat()
+
+        if (icon != null) {
+            icon.setBounds(0, 0, iconSize, iconSize)
+            markerWidth += icon.bounds.width()
+            textX += icon.bounds.width()
+        }
+
         val bitmap = Bitmap.createBitmap(
-                textBounds.width() + padding * 2,
+                markerWidth,
                 textBounds.height() + padding * 2,
                 Bitmap.Config.ARGB_8888
         )
         val canvas = Canvas(bitmap)
 
-        val y = canvas.clipBounds.height() / 2f + textBounds.height() / 2f - textBounds.bottom
+        val textY = canvas.clipBounds.height() / 2f + textBounds.height() / 2f - textBounds.bottom
 
-        //todo: remove when finished implementing marker drawing, only for debugging
-        val debugPaint = Paint()
-        debugPaint.color = Color.RED
-        canvas.drawRect(0f, 0f, canvas.clipBounds.width().toFloat(),
-                canvas.clipBounds.height().toFloat(), debugPaint)
+        canvas.drawText(title, textX, textY, textOutlinePaint)
+        canvas.drawText(title, textX, textY, textFillPaint)
 
-        canvas.drawText(title, padding.toFloat(), y, textOutlinePaint)
-        canvas.drawText(title, padding.toFloat(), y, textFillPaint)
+        if (icon != null) {
+            icon.mutate().setColorFilter(iconColor, PorterDuff.Mode.SRC_IN)
+            icon.draw(canvas)
+        }
 
         return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
