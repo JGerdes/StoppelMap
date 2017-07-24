5bdb39393ee4a5e4118b42c856dd899afc576901
Jonas Gerdes <dev@jonasgerdes.com>
2017-07-24 20:25:43 +0200

    Implement search for product names

--- app/src/main/java/com/jonasgerdes/stoppelmap/model/entity/Product.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/model/entity/Product.kt
@@ -8,6 +8,6 @@ import io.realm.RealmObject
  * @since 16.06.2017
  */
 open class Product : RealmObject() {
-    var name: String? = null
+    var name: String = "" //will be set via realm anyways, rather empty than nullable
     var prices: RealmList<Price> = RealmList()
 }
