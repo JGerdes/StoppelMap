943be1fabe861da3cebd9cc03e5b52b5b38cd1bc
Jonas Gerdes <dev@jonasgerdes.com>
2017-07-24 20:25:43 +0200

    Implement search for product names

--- app/src/main/java/com/jonasgerdes/stoppelmap/model/MapEntityRepository.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/model/MapEntityRepository.kt
@@ -2,11 +2,14 @@ package com.jonasgerdes.stoppelmap.model
 
 import com.google.android.gms.maps.model.LatLng
 import com.jonasgerdes.stoppelmap.Settings
+import com.jonasgerdes.stoppelmap.model.entity.Product
 import com.jonasgerdes.stoppelmap.model.entity.map.MapEntity
 import com.jonasgerdes.stoppelmap.model.entity.map.search.MapSearchResult
+import com.jonasgerdes.stoppelmap.model.entity.map.search.ProductSearchResult
 import com.jonasgerdes.stoppelmap.model.entity.map.search.SingleEntitySearchResult
 import com.jonasgerdes.stoppelmap.util.asList
 import com.jonasgerdes.stoppelmap.util.asRxObservable
+import com.jonasgerdes.stoppelmap.util.asset.Assets
 import com.jonasgerdes.stoppelmap.util.map.isIn
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
@@ -37,7 +40,12 @@ class MapEntityRepository : Disposable {
                             .like("alias.value", "*$term*", Case.INSENSITIVE)
                             .findAllAsync()
                             .asRxObservable()
-                            .map { entities -> createEntityResult(entities, term) }
+                            .map { entities -> createEntityResult(entities, term) },
+                    realm.where(Product::class.java)
+                            .like("name", "*$term*", Case.INSENSITIVE)
+                            .findAllAsync()
+                            .asRxObservable()
+                            .map { products -> createProductResult(products, term) }
             ).zip {
                 val result = ArrayList<MapSearchResult>()
                 it.forEach {
@@ -48,20 +56,33 @@ class MapEntityRepository : Disposable {
         }
     }
 
+    private fun createProductResult(products: List<Product>?, term: String)
+            : List<MapSearchResult> {
+        if (products == null) {
+            return emptyList()
+        }
+        return products.map { product ->
+            ProductSearchResult(product,
+                    Assets.getIconsFor(product),
+                    realm.where(MapEntity::class.java)
+                            .like("foodStall.dishes.name", product.name).findAll()
+            )
+        }
+    }
+
     private fun createEntityResult(entities: RealmResults<MapEntity>?, term: String)
-            : List<SingleEntitySearchResult> {
+            : List<MapSearchResult> {
         if (entities == null) {
             return emptyList()
         }
-        val result = ArrayList<SingleEntitySearchResult>()
-        entities.forEach { mapEntity ->
-            result.add(SingleEntitySearchResult(
+        return entities.map { mapEntity ->
+            SingleEntitySearchResult(
                     mapEntity.name!!,
                     mapEntity,
-                    mapEntity.alias.where().like("value", "*$term*", Case.INSENSITIVE).findFirst()?.value
-            ))
+                    mapEntity.alias.where()
+                            .like("value", "*$term*", Case.INSENSITIVE).findFirst()?.value
+            )
         }
-        return result
     }
 
     override fun isDisposed(): Boolean {
