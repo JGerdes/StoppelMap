b8e8105b3f801952ec21f72ffe9ac393ef55a28d
Jonas Gerdes <dev@jonasgerdes.com>
2017-08-05 10:57:38 +0200

    Create information page with licenses

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/model/entity/License.kt
@@ -0,0 +1,8 @@
+package com.jonasgerdes.stoppelmap.model.entity
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 05.08.17
+ */
+
+data class License(val title: String, val description: String)
