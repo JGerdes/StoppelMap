59e82445cda926e16efab8878efd22420ac6ca99
Jonas Gerdes <dev@jonasgerdes.com>
2017-08-09 00:21:23 +0200

    Implement version check

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/model/versioning/Release.kt
@@ -0,0 +1,10 @@
+package com.jonasgerdes.stoppelmap.model.versioning
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 08.08.17
+ */
+data class Release(
+        val name: String,
+        val code: Int
+)
