fb85d17e2edcb32513f7e20e87fdd81ad5ea4b9c
Jonas Gerdes <dev@jonasgerdes.com>
2017-08-09 00:21:23 +0200

    Implement version check

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/model/versioning/VersionInfo.kt
@@ -0,0 +1,11 @@
+package com.jonasgerdes.stoppelmap.model.versioning
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 08.08.17
+ */
+data class VersionInfo(
+        val version: HashMap<String, Release>,
+        val messages: List<Message>,
+        var currentVersion: Release? = null
+)
