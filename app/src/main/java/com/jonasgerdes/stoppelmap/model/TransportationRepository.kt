ab0fb4e99d26c93947d5ab1429c8753d802f3c97
Jonas Gerdes <dev@jonasgerdes.com>
2017-08-09 13:14:46 +0200

    Show route cards in transportation overview

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/model/TransportationRepository.kt
@@ -0,0 +1,33 @@
+package com.jonasgerdes.stoppelmap.model
+
+import com.jonasgerdes.stoppelmap.model.entity.Route
+import com.jonasgerdes.stoppelmap.util.asList
+import com.jonasgerdes.stoppelmap.util.asRxObservable
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import io.realm.Realm
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 09.08.2017
+ */
+class TransportationRepository : Disposable {
+
+    private val realm: Realm = Realm.getDefaultInstance()
+
+    fun getRoutes(): Observable<List<Route>> {
+        return realm.where(Route::class.java)
+                .findAll()
+                .asRxObservable()
+                .map { it.asList() }
+    }
+
+    override fun isDisposed(): Boolean {
+        return realm.isClosed
+    }
+
+    override fun dispose() {
+        realm.close()
+    }
+
+}
--- app/src/main/java/com/jonasgerdes/stoppelmap/usecase/transportation/overview/view/TransportOverviewFragment.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecase/transportation/overview/view/TransportOverviewFragment.kt
@@ -5,15 +5,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jonasgerdes.stoppelmap.App
 import com.jonasgerdes.stoppelmap.R
+import com.jonasgerdes.stoppelmap.model.TransportationRepository
+import com.jonasgerdes.stoppelmap.usecase.map.view.search.RouteAdapter
+import com.jonasgerdes.stoppelmap.util.plusAssign
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.transportation_overview_fragment.*
+import javax.inject.Inject
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
  * @since 22.06.2017
  */
 class TransportOverviewFragment : LifecycleFragment() {
+
+    //todo: refactor in presenter and interactor, there is no time right now...
+
+    @Inject lateinit var repository: TransportationRepository
+    private val disposables = CompositeDisposable()
+
+    init {
+        App.graph.inject(this)
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.transportation_overview_fragment, container, false)
+    }
+
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = RouteAdapter()
+        routes.adapter = adapter
+        disposables += repository.getRoutes().subscribe {
+            adapter.routes = it
+        }
+    }
+
+    override fun onDestroy() {
+        disposables.dispose()
+        super.onDestroy()
     }
 }
