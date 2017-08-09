13574b3c74683580cb4f58a7f4660807713eea4b
Jonas Gerdes <dev@jonasgerdes.com>
2017-08-10 01:54:05 +0200

    Implement behavior of toolsbar in route details

--- app/src/main/java/com/jonasgerdes/stoppelmap/usecase/transportation/route_detail/view/RouteDetailActivity.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/usecase/transportation/route_detail/view/RouteDetailActivity.kt
@@ -2,13 +2,18 @@ package com.jonasgerdes.stoppelmap.usecase.transportation.route_detail.view
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.bumptech.glide.Glide
 import com.jonasgerdes.stoppelmap.App
 import com.jonasgerdes.stoppelmap.R
 import com.jonasgerdes.stoppelmap.model.TransportationRepository
+import com.jonasgerdes.stoppelmap.util.asset.Assets
 import kotlinx.android.synthetic.main.transportation_route_detail_activity.*
 import javax.inject.Inject
+
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
@@ -38,14 +43,49 @@ class RouteDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.transportation_route_detail_activity)
+        initToolbar()
+        initStationList()
+
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
         intent.extras?.getString(EXTRA_ROUTE_SLUG)?.let {
             repository.getRouteBy(it)?.let {
+                title = it.name
+            }
+        }
+
+        val headerPath = Assets.getAppHeader(Assets.HEADER_TRANSPORTATION_ROUTE)
+        Glide.with(this)
+                .load(Uri.parse(headerPath))
+                .centerCrop()
+                .into(header)
+    }
+
+    private fun initStationList() {
+        intent.extras?.getString(EXTRA_ROUTE_SLUG)?.let {
+            repository.getRouteBy(it)?.let {
+                title = it.name
                 stationList.adapter = StationAdapter()
                 with(stationList.adapter as StationAdapter) {
                     stations = it.stations!!
                 }
             }
         }
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
