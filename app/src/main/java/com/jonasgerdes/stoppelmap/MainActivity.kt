91a7253ae6db5e920b3d3ca138a25c0b3e9b1b1f
Jonas Gerdes <dev@jonasgerdes.com>
2018-04-19 18:50:03 +0200

    Make app strech underneath statusbar

--- app/src/main/java/com/jonasgerdes/stoppelmap/MainActivity.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/MainActivity.kt
@@ -2,8 +2,11 @@ package com.jonasgerdes.stoppelmap
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import kotlinx.android.synthetic.main.main_activity.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         mapView.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         mapView.setStyleUrl("asset://style-light.json")
         mapView.getMapAsync {
             it.uiSettings.isTiltGesturesEnabled = false
