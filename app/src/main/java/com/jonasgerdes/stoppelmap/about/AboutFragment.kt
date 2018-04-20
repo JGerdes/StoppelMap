b30559120fad2140ad253d4b5e8ecbd8befc6154
Jonas Gerdes <dev@jonasgerdes.com>
2018-04-20 19:14:25 +0200

    Disable bottom navigation item movement

--- app/src/main/java/com/jonasgerdes/stoppelmap/about/AboutFragment.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/about/AboutFragment.kt
@@ -15,7 +15,7 @@ class AboutFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.feed_fragment, container, false)
+            inflater.inflate(R.layout.about_fragment, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
