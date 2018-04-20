570460487d3504aaef18b3d84ec31bca0b993bff
Jonas Gerdes <dev@jonasgerdes.com>
2018-04-20 18:42:05 +0200

    Create multiple fragments for tabs

---
+++ app/src/main/java/com/jonasgerdes/stoppelmap/map/MapFragment.kt
@@ -0,0 +1,96 @@
+package com.jonasgerdes.stoppelmap.map
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jonasgerdes.stoppelmap.R
+import com.jonasgerdes.stoppelmap.Settings
+import com.jonasgerdes.stoppelmap.util.dp
+import com.jonasgerdes.stoppelmap.util.getColorCompat
+import com.jonasgerdes.stoppelmap.util.toggleLayoutFulscreen
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import kotlinx.android.synthetic.main.map_fragment.*
+
+/**
+ * @author Jonas Gerdes <dev@jonasgerdes.com>
+ * @since 19.04.2018
+ */
+class MapFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.map_fragment, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+        activity?.toggleLayoutFulscreen(true)
+
+        mapView.setStyleUrl("asset://style-light.json")
+        mapView.getMapAsync {
+            initMapUi(it)
+            initMapCamera(it)
+        }
+    }
+
+    private fun initMapCamera(it: MapboxMap) {
+        it.setLatLngBoundsForCameraTarget(Settings.cameraBounds)
+        it.setMinZoomPreference(Settings.minZoom)
+        it.setMaxZoomPreference(Settings.maxZoom)
+        it.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                Settings.center,
+                Settings.defaultZoom
+        ))
+    }
+
+    private fun initMapUi(it: MapboxMap) {
+        it.uiSettings.isTiltGesturesEnabled = false
+        it.uiSettings.isAttributionEnabled = false
+        it.uiSettings.isLogoEnabled = false
+        it.uiSettings.setCompassMargins(16.dp, (24 + 16).dp, 16.dp, 16.dp)
+        it.uiSettings.compassImage = context?.getDrawable(R.drawable.ic_navigation_black_24dp)
+                ?.apply {
+                    context?.getColorCompat(R.color.colorPrimary)?.let { color -> setTint(color) }
+                }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        mapView?.onPause()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        mapView?.onStop()
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        mapView?.onDestroy()
+        super.onDestroyView()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+}
