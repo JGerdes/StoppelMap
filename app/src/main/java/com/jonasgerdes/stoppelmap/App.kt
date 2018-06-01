f55155694c90e8ef6f12a4af92ec22df4b732210
Jonas Gerdes <dev@jonasgerdes.com>
2018-06-01 21:44:28 +0200

    Bump Mapbox to v6.1.3

--- app/src/main/java/com/jonasgerdes/stoppelmap/App.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/App.kt
@@ -2,8 +2,9 @@ package com.jonasgerdes.stoppelmap
 
 import android.app.Application
 import com.jonasgerdes.stoppelmap.model.entity.StoppelMapDatabase
+import com.mapbox.android.telemetry.MapboxTelemetry
+import com.mapbox.android.telemetry.TelemetryEnabler
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.services.android.telemetry.MapboxTelemetry
 
 /**
  * @author Jonas Gerdes <dev@jonasgerdes.com>
@@ -19,7 +20,7 @@ class App : Application() {
 
     private fun initMapbox() {
         Mapbox.getInstance(applicationContext, "pk.NOT_USED")
-        MapboxTelemetry.getInstance().isTelemetryEnabled = false
+        TelemetryEnabler.updateTelemetryState(TelemetryEnabler.State.DISABLED)
     }
 
 }
