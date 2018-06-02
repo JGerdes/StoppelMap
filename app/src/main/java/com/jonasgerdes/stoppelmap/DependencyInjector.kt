e60c0cc546641b0b38814e4d09310db853f18070
Jonas Gerdes <dev@jonasgerdes.com>
2018-06-02 07:56:33 +0200

    Extend dependecy injection for simple singletons

--- app/src/main/java/com/jonasgerdes/stoppelmap/DependencyInjector.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/DependencyInjector.kt
@@ -2,9 +2,15 @@
 
 package com.jonasgerdes.stoppelmap
 
-import com.jonasgerdes.stoppelmap.model.entity.StoppelMapDatabase
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.jonasgerdes.stoppelmap.model.map.StoppelMapDatabase
+import com.jonasgerdes.stoppelmap.model.news.network.StoppelMapApi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.reflect.KProperty
-
 
 inline fun <reified T> inject() = inject(T::class.java)
 
@@ -16,13 +22,41 @@ class Injector<T>(private val provide: () -> T) {
     operator fun getValue(any: Any, property: KProperty<*>) = provide()
 }
 
+private fun <T> instance(provide: () -> T): Injector<T> = Injector(provide)
+
+private val singletons = mutableMapOf<String, Any>()
+
+private inline fun <reified T : Any> singleton(crossinline provide: () -> T): Injector<T> = Injector {
+    val className = T::class.java.name
+    if (!singletons.contains(className)) {
+        singletons[className] = provide()
+    }
+    singletons[className]!! as T
+}
+
+
 private fun <T> getInstanceFor(clazz: Class<T>) = when (clazz) {
 
-    StoppelMapDatabase::class.java -> Injector {
+    StoppelMapDatabase::class.java -> instance {
         StoppelMapDatabase.database
     }
 
+    Gson::class.java -> singleton {
+        GsonBuilder().create()
+    }
 
-    else -> throw RuntimeException("Couldn't inject ")
+    StoppelMapApi::class.java -> singleton {
+        val okhttp = OkHttpClient.Builder().build()
+        Retrofit.Builder()
+                .client(okhttp)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .baseUrl("https://app.stoppelmap.de/")
+                .build()
+                .create(StoppelMapApi::class.java)
+
+    }
+
+
+    else -> throw RuntimeException("Couldn't inject ${clazz.name}")
 }
-
