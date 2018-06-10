75431801f94415ec860842e46d86273446480d8c
Jonas Gerdes <dev@jonasgerdes.com>
2018-06-10 12:31:27 +0200

    Fix crash on search with spaces

--- app/src/main/java/com/jonasgerdes/stoppelmap/model/map/search/SearchResult.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/model/map/search/SearchResult.kt
@@ -3,19 +3,19 @@ package com.jonasgerdes.stoppelmap.model.map.search
 import android.support.v7.util.DiffUtil
 import com.jonasgerdes.stoppelmap.model.map.entity.Stall
 
-sealed class SearchResult(val id: String) {
+sealed class SearchResult(val id: String, val highlights: List<Highlight>) {
 
     data class SingleStallResult(
             val stall: Stall,
             val title: HighlightedText
-    ) : SearchResult(stall.slug)
+    ) : SearchResult(stall.slug, title.highlights)
 
     object DiffCallback : DiffUtil.ItemCallback<SearchResult>() {
         override fun areItemsTheSame(oldItem: SearchResult, newItem: SearchResult) =
                 oldItem.id == newItem.id
 
         override fun areContentsTheSame(oldItem: SearchResult, newItem: SearchResult) =
-                oldItem.id == newItem.id
+                oldItem.highlights == newItem.highlights
 
     }
 
@@ -28,9 +28,10 @@ data class Highlight(val start: Int, val length: Int) {
 data class HighlightedText(val text: String, val highlights: List<Highlight>) {
     companion object {
         fun from(text: String, parts: List<String>): HighlightedText {
-            val highlights = parts.foldRight(mutableListOf<Highlight>(), { part, highlights ->
-                val start = text.indexOf(part, highlights.lastOrNull()?.end ?: 0, true)
-                highlights.add(Highlight(start, part.length))
+            val highlights = parts.fold(mutableListOf<Highlight>(), { highlights, part ->
+                val start = text.indexOf(part, (highlights.lastOrNull()?.end ?: 0), true)
+                val element = Highlight(start, part.length)
+                highlights.add(element)
                 highlights
             })
             return HighlightedText(text, highlights)
