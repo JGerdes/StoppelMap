300b1abec8d4d471b8ef1c042ae0af27b48b20de
Jonas Gerdes <dev@jonasgerdes.com>
2018-07-05 22:53:56 +0200

    Change fragment handling

--- app/src/main/java/com/jonasgerdes/stoppelmap/MainActivity.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/MainActivity.kt
@@ -3,7 +3,6 @@ package com.jonasgerdes.stoppelmap
 import android.os.Bundle
 import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.jonasgerdes.stoppelmap.about.AboutFragment
 import com.jonasgerdes.stoppelmap.event.list.BusListFragment
 import com.jonasgerdes.stoppelmap.event.list.EventListFragment
@@ -25,14 +24,12 @@ class MainActivity : AppCompatActivity() {
             R.id.main_navigation_about to AboutFragment()
     )
 
-    private var lastId: Int = -1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         initNavigation()
         toggleLayoutFullscreen(true)
-        showFragment(lastId, R.id.navigation_map)
+        showFragment(R.id.navigation_map)
 
     }
 
@@ -40,26 +37,15 @@ class MainActivity : AppCompatActivity() {
         navigation.enableItemShifting(false)
         navigation.enableItemTextHiding(true)
         navigation.setOnNavigationItemSelectedListener {
-            showFragment(lastId, it.itemId)
-            lastId = it.itemId
+            showFragment(it.itemId)
             true
         }
 
     }
 
-    private fun showFragment(from: Int, to: Int) {
+    private fun showFragment(to: Int) {
         val transaction = supportFragmentManager.beginTransaction()
-
-        if (from != -1) {
-            transaction.hide(fragments[from])
-        }
-        var fragment = supportFragmentManager.findFragmentByTag(to.toString()+ resources.configuration.orientation)
-        if (fragment == null) {
-            fragment = fragments[to]
-            transaction.add(R.id.content, fragment, to.toString()+ resources.configuration.orientation)
-        } else {
-            transaction.show(fragments[to])
-        }
+        transaction.replace(R.id.content, fragments[to])
         transaction.commit()
         ViewCompat.requestApplyInsets(content)
     }
