24daa64599836c7b71edf2c948227d98a866fd8c
Jonas Gerdes <dev@jonasgerdes.com>
2018-07-15 18:38:26 +0200

    Fill about page with cards

--- app/src/main/java/com/jonasgerdes/stoppelmap/about/LibraryCard.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/about/LibraryCard.kt
@@ -4,22 +4,32 @@ import com.jonasgerdes.stoppelmap.R
 import com.jonasgerdes.stoppelmap.util.setTextOrHide
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
-import kotlinx.android.synthetic.main.about_card_author.*
+import kotlinx.android.synthetic.main.about_card_library.*
+import kotlinx.android.synthetic.main.about_card_library.view.*
 
 data class LibraryCard(
         val name: String,
         val author: String,
-        val license: String,
+        val license: License,
         val githubUrl: String? = null
 ) : Item() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.name.text = name
-        viewHolder.work.text = work
-        viewHolder.urlWebsite.setTextOrHide(website)
+        viewHolder.author.text = author
+        viewHolder.license.text = license.name
         viewHolder.urlGitHub.setTextOrHide(githubUrl)
-        viewHolder.mail.setTextOrHide(mail)
     }
 
-    override fun getLayout() = R.layout.about_card_author
+    override fun getLayout() = R.layout.about_card_library
 
+}
+
+sealed class License(
+        val name: String,
+        val fullText: String,
+        val link: String
+) {
+
+    data class Apache2(val url:String = "") : License("Apache 2", "", url)
+    data class MIT(val url:String = "") : License("MIT", "", url)
 }
