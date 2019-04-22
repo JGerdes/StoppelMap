ec7c20f0bb49c6a87c976e468623b0cbbf54e594
Jonas Gerdes <dev@jonasgerdes.com>
2019-04-22 16:27:40 +0200

    Implement splash screen

---
+++ splash/src/main/java/com/jonasgerdes/stoppelmap/splash/SplashActivity.kt
@@ -0,0 +1,7 @@
+package com.jonasgerdes.stoppelmap.splash
+
+import com.jonasgerdes.stoppelmap.core.widget.BaseActivity
+
+class SplashActivity : BaseActivity(layout = R.layout.activity_splash_screen) {
+
+}
