238813cd4d31b786f8a44060f7c598c73231e8fa
Jonas Gerdes <dev@jonasgerdes.com>
2019-05-15 07:10:36 +0200

    Keep two tone color in unselected bottom nav items

--- app/src/main/java/com/jonasgerdes/stoppelmap/view/StoppelMapActivity.kt
+++ app/src/main/java/com/jonasgerdes/stoppelmap/view/StoppelMapActivity.kt
@@ -3,11 +3,14 @@ package com.jonasgerdes.stoppelmap.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jonasgerdes.stoppelmap.R
+import kotlinx.android.synthetic.main.activity_stoppelmap.*
 
-class StoppelMap : AppCompatActivity() {
+class StoppelMapActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stoppelmap)
+
+        navigation.itemIconTintList = null
     }
 }
