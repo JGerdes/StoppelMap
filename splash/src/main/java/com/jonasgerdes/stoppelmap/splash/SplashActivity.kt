b634fc370ac655118095847b735bfbdd83031d0e
Jonas Gerdes <dev@jonasgerdes.com>
2019-05-27 17:17:18 +0200

    Integrate splash theme in main activity

--- splash/src/main/java/com/jonasgerdes/stoppelmap/splash/SplashActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.jonasgerdes.stoppelmap.splash
-
-import com.jonasgerdes.stoppelmap.core.widget.BaseActivity
-
-class SplashActivity : BaseActivity(layout = R.layout.activity_splash_screen) {
-
-}
