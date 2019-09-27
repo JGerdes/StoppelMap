3983beb726f5ab9f473af303bf438d94bfdd4050
Jonas Gerdes <dev@jonasgerdes.com>
2019-09-27 18:52:03 +0200

    Fix color from pickers always white

--- widgets/src/main/java/com/jonasgerdes/stoppelmap/widget/util/MathUtil.java
+++ widgets/src/main/java/com/jonasgerdes/stoppelmap/widget/util/MathUtil.java
@@ -3,6 +3,6 @@ package com.jonasgerdes.stoppelmap.widget.util;
 public class MathUtil {
 
     public static float limit(float lowerLimit, float upperLimit, float value) {
-        return Math.max(lowerLimit, Math.max(upperLimit, value));
+        return Math.max(lowerLimit, Math.min(upperLimit, value));
     }
 }
