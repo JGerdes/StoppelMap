0aa8a5b13e9464fab6fdae2c31f86f3be8349f03
Jonas Gerdes <dev@jonasgerdes.com>
2022-06-23 22:47:36 +0200

    Remove legacy project files

--- app/src/main/java/com/jonasgerdes/stoppelmap/model/version/Version.java
+++
@@ -1,9 +0,0 @@
-package com.jonasgerdes.stoppelmap.model.version;
-
-/**
- * Created by Jonas on 20.07.2016.
- */
-public class Version {
-    public String name;
-    public int code;
-}
--- app/src/main/java/com/jonasgerdes/stoppelmap/model/version/VersionData.java
+++
@@ -1,9 +0,0 @@
-package com.jonasgerdes.stoppelmap.model.version;
-
-/**
- * Created by Jonas on 20.07.2016.
- */
-public class VersionData {
-    public Version release;
-    public Version beta;
-}
