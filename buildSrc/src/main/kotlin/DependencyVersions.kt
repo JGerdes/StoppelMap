b02ef1b6c571c0a96a37cba6840b1b29648958c5
Jonas Gerdes <dev@jonasgerdes.com>
2022-07-03 21:06:15 +0200

    Update gradle and AGP

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -1,5 +1,5 @@
 object DependencyVersions {
-    const val gradlePlugin = "7.4.0-alpha05"
+    const val gradlePlugin = "7.4.0-alpha07"
     const val kotlin = "1.6.21"
     const val androidxCore = "1.7.0"
 }
