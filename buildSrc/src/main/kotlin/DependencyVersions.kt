d5e0e4012f5f8b0a6f691595f682f33221df8faf
Jonas Gerdes <dev@jonasgerdes.com>
2022-07-24 23:13:16 +0200

    Update compose navigation version

--- buildSrc/src/main/kotlin/DependencyVersions.kt
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -8,7 +8,7 @@ object DependencyVersions {
     const val compose = "1.2.0-rc02"
     const val material3 = "1.0.0-alpha12"
     const val androidxActivityCompose = "1.4.0"
-    const val androidxNavigation = "2.5.0-rc02"
+    const val androidxNavigation = "2.5.0"
     const val accompoanist = "0.24.13-rc"
     const val koin = "3.1.6"
     const val timber = "5.0.1"
