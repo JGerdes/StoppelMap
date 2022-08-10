b5c76b6305e5a1da474ba2b920e9d8965b83a230
Jonas Gerdes <dev@jonasgerdes.com>
2022-08-10 02:30:27 +0200

    Increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.jonasgerdes.stoppelmap"
         minSdk = SdkVersions.minSdk
         targetSdk = SdkVersions.targetSdk
-        versionCode = 45
-        versionName = "v2022.08.07.01-$commitSha"
+        versionCode = 47
+        versionName = "v2022.08.10.01-$commitSha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
