fa71931da148ced0916b2cacef5d887b48e53975
Jonas Gerdes <dev@jonasgerdes.com>
2023-08-09 00:22:25 +0200

    Implement search redesign

--- android/feature-map/src/main/kotlin/com/jonasgerdes/stoppelmap/map/ui/MapScreen.kt
+++ android/feature-map/src/main/kotlin/com/jonasgerdes/stoppelmap/map/ui/MapScreen.kt
@@ -13,34 +13,25 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.Error
 import androidx.compose.material.icons.rounded.MyLocation
-import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -48,18 +39,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.jonasgerdes.stoppelmap.base.contract.MapDataFile
 import com.jonasgerdes.stoppelmap.map.components.MapboxMap
-import com.jonasgerdes.stoppelmap.map.model.SearchResult
-import com.jonasgerdes.stoppelmap.map.ui.MapViewModel.SearchState
 import org.koin.androidx.compose.get
 import org.koin.androidx.compose.koinViewModel
 import timber.log.Timber
@@ -67,6 +50,7 @@ import timber.log.Timber
 @Composable
 fun MapScreen(
     modifier: Modifier = Modifier,
+    scaffoldPadding: PaddingValues,
     onRequestLocationPermission: () -> Unit,
     viewModel: MapViewModel = koinViewModel(),
 ) {
@@ -80,7 +64,9 @@ fun MapScreen(
             onStallTap = viewModel::onStallTapped,
             mapDataFile = "file://${mapDataFile.mapDataFile.absolutePath}".also { Timber.d("mapFile: $it") },
             colors = state.mapTheme.toMapColors(),
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(scaffoldPadding)
         )
         FloatingActionButton(
             onClick = {
@@ -90,18 +76,23 @@ fun MapScreen(
             shape = CircleShape,
             modifier = Modifier
                 .padding(16.dp)
+                .padding(scaffoldPadding)
                 .align(Alignment.BottomEnd)
         ) {
             Icon(Icons.Rounded.MyLocation, contentDescription = "Lokalisiere mich")
         }
 
         Search(
-            searchState = state.searchState,
+            query = (state.searchState as? MapViewModel.SearchState.Search)?.query ?: "",
+            results = (state.searchState as? MapViewModel.SearchState.Search)?.results
+                ?: emptyList(),
+            searchState = if (state.searchState is MapViewModel.SearchState.Search) SearchState.Expanded else SearchState.Collapsed,
             onSearchButtonTap = viewModel::onSearchButtonTapped,
             onCloseSearchTap = viewModel::onCloseSearchTapped,
             onQueryChange = viewModel::onSearchQueryChanged,
             onResultTap = viewModel::onSearchResultTapped,
-            modifier = Modifier.statusBarsPadding()
+            collapsedPaddingBottom = 56.dp + 32.dp + scaffoldPadding.calculateBottomPadding(),
+            modifier = Modifier.align(Alignment.BottomEnd),
         )
 
         var snackbarText by remember { mutableStateOf("") }
@@ -138,163 +129,3 @@ fun MapScreen(
         }
     }
 }
-
-@Composable
-fun Search(
-    searchState: SearchState,
-    onSearchButtonTap: () -> Unit,
-    onCloseSearchTap: () -> Unit,
-    onQueryChange: (String) -> Unit,
-    onResultTap: (SearchResult) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    ElevatedCard(
-        modifier = modifier.padding(16.dp)
-    ) {
-        //AnimatedContent(targetState = searchState) { searchState ->
-        when (searchState) {
-            SearchState.Collapsed -> SearchPill(onTap = onSearchButtonTap)
-            is SearchState.Search -> SearchField(
-                searchState = searchState,
-                onQueryChange = onQueryChange,
-                onResultTap = onResultTap,
-                onCloseSearchTap = onCloseSearchTap
-            )
-
-            is SearchState.HighlightResult -> HighlightedCard(
-                searchState = searchState,
-                onCloseSearchTap = onCloseSearchTap
-            )
-        }
-        //}
-    }
-}
-
-@Composable
-fun HighlightedCard(
-    searchState: SearchState.HighlightResult,
-    onCloseSearchTap: () -> Unit
-) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
-    ) {
-        val context = LocalContext.current
-        val vectorId = context.getDrawableForStallType(searchState.result.stalls.first().type)
-        if (vectorId == null) {
-            Spacer(modifier = Modifier.size(24.dp))
-        } else {
-            Icon(
-                painterResource(id = vectorId),
-                contentDescription = null
-            )
-        }
-        Spacer(modifier = Modifier.size(8.dp))
-        Text(text = searchState.result.term)
-        Spacer(
-            modifier = Modifier
-                .defaultMinSize(minWidth = 8.dp)
-                .weight(1f)
-        )
-        IconButton(
-            onClick = { onCloseSearchTap() }
-        ) {
-            Icon(Icons.Rounded.Close, contentDescription = null)
-        }
-    }
-}
-
-@Composable
-fun SearchField(
-    searchState: SearchState.Search,
-    onQueryChange: (String) -> Unit,
-    onResultTap: (SearchResult) -> Unit,
-    onCloseSearchTap: () -> Unit
-) {
-    val focusRequester = remember { FocusRequester() }
-    Column(Modifier.fillMaxWidth()) {
-        TextField(
-            value = searchState.query,
-            onValueChange = onQueryChange,
-            trailingIcon = {
-                IconButton(onClick = { onCloseSearchTap() }) {
-                    Icon(Icons.Rounded.Close, contentDescription = null)
-                }
-            },
-            placeholder = { Text("Suche") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .focusRequester(focusRequester)
-                .onGloballyPositioned {
-                    focusRequester.requestFocus()
-                }
-        )
-        if (searchState.results.isNotEmpty() || searchState.query.isBlank()) {
-            LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                items(searchState.results) { result ->
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                onResultTap(result)
-                            }
-                            .padding(16.dp)
-                    ) {
-                        val context = LocalContext.current
-                        val vectorId = context.getDrawableForStallType(result.stalls.first().type)
-                        if (vectorId == null) {
-                            Spacer(modifier = Modifier.size(24.dp))
-                        } else {
-                            Icon(
-                                painterResource(id = vectorId),
-                                contentDescription = null
-                            )
-                        }
-                        Spacer(modifier = Modifier.size(8.dp))
-                        Text(text = result.term)
-                    }
-                }
-            }
-        } else {
-            Text(
-                text = "Keine Ergebnisse zur Suche nach \"${searchState.query}\"",
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(32.dp)
-            )
-        }
-    }
-}
-
-
-@Composable
-fun SearchPill(
-    onTap: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Row(modifier
-            .clickable { onTap() }
-            .padding(vertical = 8.dp, horizontal = 16.dp)) {
-        Icon(Icons.Rounded.Search, contentDescription = null)
-        Spacer(modifier = Modifier.size(8.dp))
-        Text("Suche")
-    }
-}
-
-@DrawableRes
-private fun Context.getDrawableForStallType(stallType: String): Int? =
-    "ic_stall_type_${stallType.replace("-", "_")}".let { name ->
-        val id = resources.getIdentifier(
-            name,
-            "drawable",
-            packageName
-        )
-        if (id == 0) {
-            Timber.w("Can't find icon for type [$stallType]")
-            null
-        } else id
-    }
---
+++ android/feature-map/src/main/kotlin/com/jonasgerdes/stoppelmap/map/ui/Search.kt
@@ -0,0 +1,232 @@
+package com.jonasgerdes.stoppelmap.map.ui
+
+import android.content.Context
+import androidx.activity.compose.BackHandler
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.jonasgerdes.stoppelmap.map.model.SearchResult
+import kotlinx.coroutines.delay
+import timber.log.Timber
+
+@Suppress("AnimateAsStateLabel")
+@Composable
+fun Search(
+    query: String,
+    results: List<SearchResult>,
+    searchState: SearchState,
+    onSearchButtonTap: () -> Unit,
+    onCloseSearchTap: () -> Unit,
+    onQueryChange: (String) -> Unit,
+    onResultTap: (SearchResult) -> Unit,
+    collapsedPaddingBottom: Dp,
+    modifier: Modifier = Modifier,
+) {
+    val backgroundColor by animateColorAsState(
+        when (searchState) {
+            SearchState.Collapsed -> MaterialTheme.colorScheme.primaryContainer
+            SearchState.Expanded -> MaterialTheme.colorScheme.surfaceColorAtElevation(6.dp)
+        },
+        spring(stiffness = Spring.StiffnessMediumLow)
+    )
+    val cornerSize by animateDpAsState(
+        when (searchState) {
+            SearchState.Collapsed -> 56.dp
+            SearchState.Expanded -> 0.dp
+        },
+        spring(stiffness = Spring.StiffnessMediumLow)
+    )
+    val paddingBottom by animateDpAsState(
+        when (searchState) {
+            SearchState.Collapsed -> collapsedPaddingBottom
+            SearchState.Expanded -> 0.dp
+        },
+        spring(stiffness = Spring.StiffnessMediumLow)
+    )
+    val paddingEnd by animateDpAsState(
+        when (searchState) {
+            SearchState.Collapsed -> 16.dp
+            SearchState.Expanded -> 0.dp
+        },
+        spring(stiffness = Spring.StiffnessMediumLow)
+    )
+    val elevation by animateDpAsState(
+        when (searchState) {
+            SearchState.Collapsed -> 6.dp
+            SearchState.Expanded -> 0.dp
+        },
+    )
+    val focusRequester = remember { FocusRequester() }
+    Box(
+        modifier = modifier
+            .padding(bottom = paddingBottom, end = paddingEnd)
+            .shadow(elevation = elevation, RoundedCornerShape(cornerSize))
+            .background(backgroundColor, RoundedCornerShape(cornerSize))
+            .animateContentSize(
+                spring(stiffness = Spring.StiffnessMediumLow)
+            )
+    ) {
+        Column(
+            modifier = when (searchState) {
+                SearchState.Collapsed -> modifier.size(56.dp)
+                SearchState.Expanded -> modifier
+                    .fillMaxSize()
+                    .statusBarsPadding()
+                    .imePadding()
+            }
+        ) {
+            Row {
+                IconButton(
+                    onClick = { if (searchState == SearchState.Collapsed) onSearchButtonTap() else onCloseSearchTap() },
+                    modifier = Modifier.size(56.dp)
+                ) {
+                    Icon(
+                        if (searchState == SearchState.Collapsed) Icons.Rounded.Search else Icons.Rounded.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+                TextField(
+                    value = query,
+                    onValueChange = onQueryChange,
+                    trailingIcon = {
+                        IconButton(onClick = { onQueryChange("") }) {
+                            Icon(Icons.Rounded.Close, contentDescription = null)
+                        }
+                    },
+                    placeholder = { Text("Auf der Karte suchen") },
+                    colors = TextFieldDefaults.colors(
+                        disabledContainerColor = Color.Transparent,
+                        errorContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        errorIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .weight(1f)
+                        .focusRequester(focusRequester)
+                )
+            }
+            Divider()
+            if (results.isNotEmpty() || query.isBlank()) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    items(results) { result ->
+                        ListItem(
+                            headlineContent = { Text(text = result.term) },
+                            leadingContent = {
+                                val context = LocalContext.current
+                                val vectorId =
+                                    context.getDrawableForStallType(result.stalls.first().type)
+                                if (vectorId == null) {
+                                    Spacer(modifier = Modifier.size(24.dp))
+                                } else {
+                                    Icon(
+                                        painterResource(id = vectorId),
+                                        contentDescription = null
+                                    )
+                                }
+                            },
+                            tonalElevation = 6.dp,
+                            modifier = Modifier.clickable {
+                                onResultTap(result)
+                            }
+                        )
+                    }
+                }
+            } else {
+                Text(
+                    text = "Keine Ergebnisse zur Suche nach \"${query}\"",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(32.dp)
+                )
+            }
+        }
+        val focusManager = LocalFocusManager.current
+        LaunchedEffect(searchState, focusManager) {
+            delay(200)
+            when (searchState) {
+                SearchState.Collapsed -> focusManager.clearFocus()
+                SearchState.Expanded -> focusRequester.requestFocus()
+            }
+        }
+        DisposableEffect(focusManager) {
+            onDispose {
+                focusManager.clearFocus()
+            }
+        }
+        BackHandler(enabled = searchState == SearchState.Expanded) {
+            onCloseSearchTap()
+        }
+    }
+}
+
+enum class SearchState {
+    Collapsed, Expanded
+}
+
+@DrawableRes
+private fun Context.getDrawableForStallType(stallType: String): Int? =
+    "ic_stall_type_${stallType.replace("-", "_")}".let { name ->
+        val id = resources.getIdentifier(
+            name,
+            "drawable",
+            packageName
+        )
+        if (id == 0) {
+            Timber.w("Can't find icon for type [$stallType]")
+            null
+        } else id
+    }
