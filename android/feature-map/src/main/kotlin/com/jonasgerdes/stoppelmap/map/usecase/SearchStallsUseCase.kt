1a88cff77ec756822deade4ab5a0679734b82580
Jonas Gerdes <dev@jonasgerdes.com>
2023-08-11 01:49:23 +0200

    Improve sorting of search results

--- android/feature-map/src/main/kotlin/com/jonasgerdes/stoppelmap/map/usecase/SearchStallsUseCase.kt
+++ android/feature-map/src/main/kotlin/com/jonasgerdes/stoppelmap/map/usecase/SearchStallsUseCase.kt
@@ -42,6 +42,31 @@ class SearchStallsUseCase(
             }
         }
 
-        return (stallResults + aliasResults + typeResults + itemResults).filter { it.stalls.isNotEmpty() }
+        val cleanQuery = query.trim().lowercase()
+
+        return (stallResults + aliasResults + typeResults + itemResults)
+            .filter { it.stalls.isNotEmpty() }
+            .map {
+                it withScore when {
+                    it.stalls.size == 1 && it.term.lowercase().startsWith(cleanQuery) -> 1f
+                    it.term.lowercase().startsWith(cleanQuery) -> 0.5f
+                    else -> 0f
+                }
+            }
+            .sortedWith { first, second ->
+                when {
+                    first.score == second.score && first.searchResult.stalls.size == second.searchResult.stalls.size -> first.searchResult.term.compareTo(
+                        second.searchResult.term
+                    )
+
+                    first.score == second.score -> first.searchResult.stalls.size.compareTo(second.searchResult.stalls.size) * -1// Stall count: Higher = better
+                    else -> first.score.compareTo(second.score) * -1// Score: Higher = better
+                }
+            }
+            .map { it.searchResult }
     }
 }
+
+private data class ScoredResult(val searchResult: SearchResult, val score: Float)
+
+private infix fun SearchResult.withScore(score: Float) = ScoredResult(this, score)
