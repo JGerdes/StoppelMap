a705acd466949e2504978742c1ff1483927db3d9
Jonas Gerdes <dev@jonasgerdes.com>
2024-04-02 22:47:12 +0200

    Improve design of event page

--- iosApp/iosApp/schedule/EventPage.swift
+++ iosApp/iosApp/schedule/EventPage.swift
@@ -9,42 +9,75 @@ struct EventPage: View {
     
     var body: some View {
         ScrollView(.vertical) {
-            VStack(spacing: 0) {
+            LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
                 ForEach(0..<scheduleDay.timeSlots.count, id: \.self) { timeSlotIndex in
                     let slot = scheduleDay.timeSlots[timeSlotIndex]
-                    ForEach(0..<slot.events.count, id: \.self) { eventIndex in
-                        let event = slot.events[eventIndex]
-                        HStack(alignment: .top, spacing: 0) {
-                            ZStack {
-                                Text("00:00")
-                                    .font(.title3)
-                                    .padding()
-                                    .hidden()
-                                if(eventIndex == 0) {
-                                    Text(slot.time.description())
-                                        .font(.title3)
-                                        .padding()
-                                }
+                    TimeSlot(
+                        slot: slot,
+                        selectedEvent: selectedEvent,
+                        onEventTap: onEventTap,
+                        onNotificationToggle: onNotificationToggle
+                    )
+                }
+            }
+        }
+    }
+}
+
+struct SectionHeader: View {
+    
+    var slot: ScheduleTime
+    
+    var body: some View {
+        ZStack {
+            Text(slot.time.description())
+                .font(.title3)
+                .padding([.top, .horizontal])
+        }
+        .background(.background)
+        .frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            maxHeight: 1,
+            alignment: .topLeading
+        )
+    }
+}
+
+struct TimeSlot: View {
+    
+    var slot: ScheduleTime
+    var selectedEvent: DataEvent?
+    var onEventTap: (DataEvent) -> ()
+    var onNotificationToggle: (_ event: DataEvent, _ isActive: Bool) -> ()
+    
+    var body: some View {
+        Section(header: SectionHeader(slot: slot).listRowInsets(.none)) {
+            ForEach(slot.events, id: \.event.slug) { event in
+                HStack(alignment: .top, spacing: 0) {
+                    ZStack {
+                        Text("00:00")
+                            .font(.title3)
+                            .padding()
+                            .hidden()
+                        //                                    if(eventIndex == 0) {
+                        //                                        Text(slot.time.description())
+                        //                                            .font(.title3)
+                        //                                            .padding()
+                        //                                    }
+                    }
+                    VStack(spacing: 0) {
+                        EventRow(
+                            event: event,
+                            selected: event.event == selectedEvent,
+                            onSelect: {
+                                onEventTap(event.event)
+                            },
+                            onNotificationToggle: { active in
+                                onNotificationToggle(event.event, active)
                             }
-                            VStack {
-                                EventRow(
-                                    event: event,
-                                    selected: event.event == selectedEvent,
-                                    onSelect: {
-                                        onEventTap(event.event)
-                                    },
-                                    onNotificationToggle: { active in
-                                        onNotificationToggle(event.event, active)
-                                    }
-                                )
-                                Divider()
-                            }
-                            
-                        }.frame(
-                            minWidth: 0,
-                            maxWidth: .infinity,
-                            alignment: .topLeading
                         )
+                        Divider()
                     }
                 }
             }
@@ -76,27 +109,28 @@ struct EventRow: View {
                 maxWidth: .infinity,
                 alignment: .topLeading
             )
-                Button(
-                    action: { onNotificationToggle(!event.bookmarked) }
-                ){
-                    Image(systemName: event.bookmarked ? "bookmark.fill" : "bookmark")
-                }
-                .opacity((selected || event.bookmarked) ? 1 : 0)
+            Button(
+                action: { onNotificationToggle(!event.bookmarked) }
+            ){
+                Image(systemName: event.bookmarked ? "bookmark.fill" : "bookmark")
+            }
+            .opacity((selected || event.bookmarked) ? 1 : 0)
         }
-        .padding()
+        .padding(.horizontal)
+        .padding(.vertical, selected ? 16 : 8)
         .frame(
             minWidth: 0,
             maxWidth: .infinity,
             alignment: .topLeading
         )
-        
+        .contentShape(Rectangle())
         .onTapGesture {
             onSelect()
         }
         .background(selected ? .accent.opacity(0.2) : .clear)
         .cornerRadius(8.0)
         .padding(.trailing)
-        .animation(.easeInOut, value: selected)
+        .padding(.vertical, 8)
+        .animation(.spring(response: 0.3), value: selected)
     }
-    
 }
