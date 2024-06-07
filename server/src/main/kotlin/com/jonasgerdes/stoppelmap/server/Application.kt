3012a1a55763e37b720dd006f89bdd8587bd8500
Jonas Gerdes <dev@jonasgerdes.com>
2024-06-07 20:49:30 +0200

    Server: Serve images statically

--- server/src/main/kotlin/com/jonasgerdes/stoppelmap/server/Application.kt
+++ server/src/main/kotlin/com/jonasgerdes/stoppelmap/server/Application.kt
@@ -7,12 +7,16 @@ import com.jonasgerdes.stoppelmap.server.data.dataModule
 import com.jonasgerdes.stoppelmap.server.scheduler.Schedule
 import com.jonasgerdes.stoppelmap.server.scheduler.Task
 import com.jonasgerdes.stoppelmap.server.scheduler.TaskScheduler
+import io.ktor.http.CacheControl
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.application.install
+import io.ktor.server.http.content.staticFiles
+import io.ktor.server.plugins.cachingheaders.CachingHeaders
 import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
+import io.ktor.server.plugins.compression.Compression
 import io.ktor.server.routing.routing
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
@@ -21,6 +25,7 @@ import org.koin.core.logger.Level
 import org.koin.dsl.module
 import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
+import java.io.File
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -28,6 +33,8 @@ fun Application.ktorModule() {
     val appConfig = environment.config.toAppConfig()
     environment.log.info("Starting with environment ${appConfig.environment}")
     install(CallLogging)
+    install(Compression)
+    install(CachingHeaders)
     install(Koin) {
         printLogger(if (appConfig.environment.isDev) Level.DEBUG else Level.INFO)
         modules(
@@ -55,6 +62,16 @@ fun Application.ktorModule() {
     routing {
         get("/") {
             call.respondText("Hello Stoppelmarkt!")
+        staticFiles("/static/images", File(appConfig.crawler.imageCacheDir, "processed")) {
+            enableAutoHeadResponse()
+            cacheControl {
+                listOf(
+                    CacheControl.MaxAge(
+                        maxAgeSeconds = 604800, // year
+                        visibility = CacheControl.Visibility.Public,
+                    )
+                )
+            }
         }
     }
 
