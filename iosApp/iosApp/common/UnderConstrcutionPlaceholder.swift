a852fd65a4985cd31dbc0c9e3378baf27e384005
Jonas Gerdes <dev@jonasgerdes.com>
2024-06-23 13:32:55 +0200

    iOS: Add under construction animation

--- iosApp/iosApp/common/UnderConstrcutionPlaceholder.swift
+++ iosApp/iosApp/common/UnderConstrcutionPlaceholder.swift
@@ -1,11 +1,30 @@
 import SwiftUI
 import Shared
+import Lottie
 
 struct UnderConstrcutionPlaceholder: View {
+    
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    
     var body: some View {
         VStack(){
+            let fillColor = if(colorScheme == .dark) {
+                LottieColor(r: 1, g: 1, b: 1, a: 1)
+            } else {
+                LottieColor(r: 0, g: 0, b: 0, a: 1)
+            }
+            
+            LottieView(animation: .named("UnderConstruction"))
+                .looping()
+                .valueProvider(ColorValueProvider(fillColor), for: AnimationKeypath(keypath: "**.Fill.Color"))
+                .configure { lottie in
+                    lottie.contentMode = .scaleAspectFit
+                }
+                .resizable()
+                .frame(width: 256, height: 256)
+                .opacity(0.8)
             Text(Res.strings().common_available_soon.desc().localized())
-                .font(.title)
+                .font(.titleSlab)
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
             .ignoresSafeArea()
             .background(.ultraThinMaterial)
