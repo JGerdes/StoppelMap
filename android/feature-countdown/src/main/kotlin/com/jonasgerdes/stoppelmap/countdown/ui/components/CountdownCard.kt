886736b519924017f100bf78f6a71fb1469272bf
Jonas Gerdes <dev@jonasgerdes.com>
2024-07-04 21:41:10 +0200

    Android: Add dark theme for countdown card

--- android/feature-countdown/src/main/kotlin/com/jonasgerdes/stoppelmap/countdown/ui/components/CountdownCard.kt
+++ android/feature-countdown/src/main/kotlin/com/jonasgerdes/stoppelmap/countdown/ui/components/CountdownCard.kt
@@ -20,13 +20,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -42,13 +43,14 @@ import com.jonasgerdes.stoppelmap.base.contract.Season
 import com.jonasgerdes.stoppelmap.countdown.R
 import com.jonasgerdes.stoppelmap.resources.dayOfMonthFormat
 import com.jonasgerdes.stoppelmap.resources.defaultFormat
-import com.jonasgerdes.stoppelmap.theme.LightColorScheme
+import com.jonasgerdes.stoppelmap.theme.LocalThemeSetting
 import com.jonasgerdes.stoppelmap.theme.StoppelMapTheme
+import com.jonasgerdes.stoppelmap.theme.isDarkTheme
 import com.jonasgerdes.stoppelmap.theme.modifier.invisible
+import com.jonasgerdes.stoppelmap.theme.settings.ThemeSetting
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.Month
-import kotlinx.datetime.format
 
 @Composable
 fun CountdownCard(
@@ -59,143 +61,159 @@ fun CountdownCard(
     season: Season,
     modifier: Modifier = Modifier
 ) {
+    val isDarkTheme = LocalThemeSetting.current.isDarkTheme()
     Card(modifier = modifier) {
-        CompositionLocalProvider(LocalContentColor provides LightColorScheme.onPrimaryContainer) {
-            // We want the illustration next to the countdown and info,
-            // the background colors matching the height of the countdown and info
-            // while also reaching beneath the illustration.
-            ConstraintLayout(modifier.fillMaxWidth()) {
-                val (barrierHelper, skyBackground, fieldBackground,
-                    illustration, countdown, footer) = createRefs()
+        // We want the illustration next to the countdown and info,
+        // the background colors matching the height of the countdown and info
+        // while also reaching beneath the illustration.
+        ConstraintLayout(modifier.fillMaxWidth()) {
+            val (barrierHelper, skyBackground, fieldBackground,
+                illustration, countdown, footer) = createRefs()
 
-                val illustrationGuideline = createGuidelineFromStart(144.dp)
-                val illustrationBarrier = createStartBarrier(countdown, barrierHelper)
+            val illustrationGuideline = createGuidelineFromStart(144.dp)
+            val illustrationBarrier = createStartBarrier(countdown, barrierHelper)
 
-                // This is just here so we can constraint the barrier to the the guideline,
-                // which isn't supported by ConstraintLayout out-of-the-box.
-                Box(Modifier.constrainAs(barrierHelper) {
-                    top.linkTo(parent.top)
-                    start.linkTo(illustrationGuideline)
-                    end.linkTo(illustrationGuideline)
-                })
-                Box(
-                    Modifier
-                        .background(colorResource(id = R.color.stoppelsky))
-                        .constrainAs(skyBackground) {
-                            top.linkTo(parent.top)
-                            end.linkTo(parent.end)
-                            start.linkTo(parent.start)
-                            bottom.linkTo(countdown.bottom)
-                            width = fillToConstraints
-                            height = fillToConstraints
-                        }
-                )
-                Box(
-                    Modifier
-                        .background(colorResource(id = R.color.stoppelfield))
-                        .constrainAs(fieldBackground) {
-                            top.linkTo(countdown.bottom)
-                            end.linkTo(parent.end)
-                            start.linkTo(parent.start)
-                            bottom.linkTo(parent.bottom)
-                            width = fillToConstraints
-                            height = fillToConstraints
-                        }
-                )
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    modifier = Modifier
-                        .constrainAs(countdown) {
-                            start.linkTo(illustrationGuideline)
-                            // Double padding of 16.dp to the end to have some additional
-                            // wiggle room for large font scaling.
-                            end.linkTo(parent.end, 16.dp)
-                            top.linkTo(parent.top)
-                            bottom.linkTo(footer.top)
-                            width = wrapContent
-                        }
-                        .width(IntrinsicSize.Min)
-                        .padding(vertical = 16.dp)
-                ) {
-                    Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                        CountdownUnit(
-                            unitLabel = R.plurals.countdownCard_unit_day,
-                            value = days,
-                            modifier = Modifier.weight(1f)
-                        )
-                        CountdownUnit(
-                            unitLabel = R.plurals.countdownCard_unit_hour,
-                            value = hours,
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-                    Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                        CountdownUnit(
-                            unitLabel = R.plurals.countdownCard_unit_minute,
-                            value = minutes,
-                            modifier = Modifier.weight(1f)
-                        )
-                        CountdownUnit(
-                            unitLabel = R.plurals.countdownCard_unit_second,
-                            value = seconds,
-                            modifier = Modifier.weight(1f)
-                        )
-                    }
-                }
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.End,
-                    modifier = Modifier
-                        .constrainAs(footer) {
-                            top.linkTo(countdown.bottom)
-                            end.linkTo(parent.end)
-                            bottom.linkTo(parent.bottom)
-                            height = wrapContent
-                            width = wrapContent
-                        }
-                        .padding(16.dp)
-                ) {
-                    Text(
-                        text = stringResource(
-                            id = R.string.countdownCard_suffix,
-                            season.year
-                        ),
-                        style = MaterialTheme.typography.bodySmall,
-                        textAlign = TextAlign.End,
+            // This is just here so we can constraint the barrier to the the guideline,
+            // which isn't supported by ConstraintLayout out-of-the-box.
+            Box(Modifier.constrainAs(barrierHelper) {
+                top.linkTo(parent.top)
+                start.linkTo(illustrationGuideline)
+                end.linkTo(illustrationGuideline)
+            })
+            Box(
+                Modifier
+                    .background(
+                        if (isDarkTheme) MaterialTheme.colorScheme.surfaceContainer
+                        else colorResource(id = R.color.stoppelsky)
                     )
-                    Text(
-                        text = stringResource(
-                            id = R.string.countdownCard_iteration,
-                            season.iteration,
-                        ),
-                        style = MaterialTheme.typography.titleLarge,
-                        textAlign = TextAlign.End,
+                    .constrainAs(skyBackground) {
+                        top.linkTo(parent.top)
+                        end.linkTo(parent.end)
+                        start.linkTo(parent.start)
+                        bottom.linkTo(countdown.bottom)
+                        width = fillToConstraints
+                        height = fillToConstraints
+                    }
+            )
+            Box(
+                Modifier
+                    .background(
+                        if (isDarkTheme) MaterialTheme.colorScheme.surfaceContainerHigh
+                        else colorResource(id = R.color.stoppelfield)
                     )
-                    val context = LocalContext.current
-                    Text(
-                        text = stringResource(
-                            id = R.string.countdownCard_dates,
-                            season.start.date.dayOfMonthFormat().toString(context),
-                            season.end.date.defaultFormat().toString(context),
-                        ),
-                        style = MaterialTheme.typography.bodySmall,
-                        textAlign = TextAlign.End,
+                    .constrainAs(fieldBackground) {
+                        top.linkTo(countdown.bottom)
+                        end.linkTo(parent.end)
+                        start.linkTo(parent.start)
+                        bottom.linkTo(parent.bottom)
+                        width = fillToConstraints
+                        height = fillToConstraints
+                    }
+            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier
+                    .constrainAs(countdown) {
+                        start.linkTo(illustrationGuideline)
+                        // Double padding of 16.dp to the end to have some additional
+                        // wiggle room for large font scaling.
+                        end.linkTo(parent.end, 16.dp)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(footer.top)
+                        width = wrapContent
+                    }
+                    .width(IntrinsicSize.Min)
+                    .padding(vertical = 16.dp)
+            ) {
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    CountdownUnit(
+                        unitLabel = R.plurals.countdownCard_unit_day,
+                        value = days,
+                        modifier = Modifier.weight(1f)
+                    )
+                    CountdownUnit(
+                        unitLabel = R.plurals.countdownCard_unit_hour,
+                        value = hours,
+                        modifier = Modifier.weight(1f)
                     )
                 }
-                Image(
-                    painter = painterResource(R.drawable.jan_libett_balloons),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .constrainAs(illustration) {
-                            start.linkTo(parent.start, 16.dp)
-                            bottom.linkTo(parent.bottom, 16.dp)
-                            end.linkTo(illustrationBarrier)
-                            top.linkTo(parent.top, 16.dp)
-                            width = fillToConstraints
-                            height = fillToConstraints
-                        }
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    CountdownUnit(
+                        unitLabel = R.plurals.countdownCard_unit_minute,
+                        value = minutes,
+                        modifier = Modifier.weight(1f)
+                    )
+                    CountdownUnit(
+                        unitLabel = R.plurals.countdownCard_unit_second,
+                        value = seconds,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.End,
+                modifier = Modifier
+                    .constrainAs(footer) {
+                        top.linkTo(countdown.bottom)
+                        end.linkTo(parent.end)
+                        bottom.linkTo(parent.bottom)
+                        height = wrapContent
+                        width = wrapContent
+                    }
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(
+                        id = R.string.countdownCard_suffix,
+                        season.year
+                    ),
+                    style = MaterialTheme.typography.bodySmall,
+                    textAlign = TextAlign.End,
+                )
+                Text(
+                    text = stringResource(
+                        id = R.string.countdownCard_iteration,
+                        season.iteration,
+                    ),
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.End,
+                )
+                val context = LocalContext.current
+                Text(
+                    text = stringResource(
+                        id = R.string.countdownCard_dates,
+                        season.start.date.dayOfMonthFormat().toString(context),
+                        season.end.date.defaultFormat().toString(context),
+                    ),
+                    style = MaterialTheme.typography.bodySmall,
+                    textAlign = TextAlign.End,
                 )
             }
+            Image(
+                painter = painterResource(R.drawable.jan_libett_balloons),
+                contentDescription = null,
+                colorFilter = if (isDarkTheme) {
+                    // Darkens the illustration a little bit, so we don't need sunglasses to look at it.
+                    ColorFilter.lighting(
+                        lerp(
+                            MaterialTheme.colorScheme.background,
+                            Color.White,
+                            0.75f
+                        ),
+                        Color.Black
+                    )
+                } else null,
+                modifier = Modifier
+                    .constrainAs(illustration) {
+                        start.linkTo(parent.start, 16.dp)
+                        bottom.linkTo(parent.bottom, 16.dp)
+                        end.linkTo(illustrationBarrier)
+                        top.linkTo(parent.top, 16.dp)
+                        width = fillToConstraints
+                        height = fillToConstraints
+                    }
+            )
         }
     }
 }
@@ -244,7 +262,7 @@ private fun CountdownUnit(
 @Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 private fun PreviewCountdownCard() {
-    StoppelMapTheme {
+    StoppelMapTheme(themeSetting = ThemeSetting.System) {
         CountdownCard(
             days = 129,
             hours = 1,
--- android/theme/src/main/kotlin/com/jonasgerdes/stoppelmap/theme/Theme.kt
+++ android/theme/src/main/kotlin/com/jonasgerdes/stoppelmap/theme/Theme.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -40,50 +42,57 @@ fun StoppelMapTheme(
     colorSchemeSetting: ColorSchemeSetting = ColorSchemeSetting.Classic,
     content: @Composable () -> Unit
 ) {
-    val darkTheme = when (themeSetting) {
-        ThemeSetting.Light -> false
-        ThemeSetting.Dark -> true
-        ThemeSetting.System -> isSystemInDarkTheme()
-    }
+    CompositionLocalProvider(LocalThemeSetting provides themeSetting) {
+        val darkTheme = LocalThemeSetting.current.isDarkTheme()
 
-    val colorScheme = when {
-        colorSchemeSetting == ColorSchemeSetting.System &&
-                Build.VERSION.SDK_INT.supportsDynamicColor() -> {
-            val context = LocalContext.current
-            if (darkTheme)
-                dynamicDarkColorScheme(context)
-            else
-                dynamicLightColorScheme(context)
+        val colorScheme = when {
+            colorSchemeSetting == ColorSchemeSetting.System &&
+                    Build.VERSION.SDK_INT.supportsDynamicColor() -> {
+                val context = LocalContext.current
+                if (darkTheme)
+                    dynamicDarkColorScheme(context)
+                else
+                    dynamicLightColorScheme(context)
+            }
+
+            colorSchemeSetting == ColorSchemeSetting.Classic -> if (darkTheme) DarkColorScheme else LightColorScheme
+            else -> if (darkTheme) DarkColorScheme else LightColorScheme
         }
 
-        colorSchemeSetting == ColorSchemeSetting.Classic -> if (darkTheme) DarkColorScheme else LightColorScheme
-        else -> if (darkTheme) DarkColorScheme else LightColorScheme
-    }
+        val view = LocalView.current
+        val fallbackSystemBarColor = MaterialTheme.colorScheme.primary.toArgb()
+        if (!view.isInEditMode) {
+            SideEffect {
+                val window = (view.context as Activity).window
+                val insetsController = WindowCompat.getInsetsController(window, view)
 
-    val view = LocalView.current
-    val fallbackSystemBarColor = MaterialTheme.colorScheme.primary.toArgb()
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            val insetsController = WindowCompat.getInsetsController(window, view)
+                window.navigationBarColor =
+                    if (Build.VERSION.SDK_INT.supportsLightNavigationBarAppearance() || darkTheme)
+                        Color.Transparent.toArgb()
+                    else fallbackSystemBarColor
 
-            window.navigationBarColor =
-                if (Build.VERSION.SDK_INT.supportsLightNavigationBarAppearance() || darkTheme)
-                    Color.Transparent.toArgb()
-                else fallbackSystemBarColor
+                window.statusBarColor =
+                    if (Build.VERSION.SDK_INT.supportsLightStatusBarAppearance() || darkTheme)
+                        Color.Transparent.toArgb()
+                    else fallbackSystemBarColor
 
-            window.statusBarColor =
-                if (Build.VERSION.SDK_INT.supportsLightStatusBarAppearance() || darkTheme)
-                    Color.Transparent.toArgb()
-                else fallbackSystemBarColor
-
-            insetsController.isAppearanceLightStatusBars = !darkTheme
-            insetsController.isAppearanceLightNavigationBars = !darkTheme
+                insetsController.isAppearanceLightStatusBars = !darkTheme
+                insetsController.isAppearanceLightNavigationBars = !darkTheme
+            }
         }
+        MaterialTheme(
+            colorScheme = colorScheme, typography = Typography, content = content
+        )
     }
-    MaterialTheme(
-        colorScheme = colorScheme, typography = Typography, content = content
-    )
 }
 
 val ColorScheme.onScrim get() = Color.White
+
+val LocalThemeSetting = compositionLocalOf { ThemeSetting.default }
+
+@Composable
+fun ThemeSetting.isDarkTheme() = when (this) {
+    ThemeSetting.Light -> false
+    ThemeSetting.Dark -> true
+    ThemeSetting.System -> isSystemInDarkTheme()
+}
