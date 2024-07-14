b486d25124c637074656a3664dae69d6bd7f191b
Jonas Gerdes <dev@jonasgerdes.com>
2024-07-14 14:18:25 +0200

    Android: Improve Widget promotion

--- android/feature-countdown/src/main/kotlin/com/jonasgerdes/stoppelmap/countdown/ui/components/CountdownWidgetSuggestionCard.kt
+++ android/feature-countdown/src/main/kotlin/com/jonasgerdes/stoppelmap/countdown/ui/components/CountdownWidgetSuggestionCard.kt
@@ -11,15 +11,25 @@ import android.os.Build
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,43 +38,59 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jonasgerdes.stoppelmap.countdown.R
 import com.jonasgerdes.stoppelmap.countdown.widget.heart.GingerbreadWidgetSettingsActivity
 import com.jonasgerdes.stoppelmap.countdown.widget.skyline.SkylineWidgetSettingsActivity
+import com.jonasgerdes.stoppelmap.theme.StoppelMapTheme
 import com.jonasgerdes.stoppelmap.widget.countdown.CountdownWidgetReceiver
 import com.jonasgerdes.stoppelmap.widget.heart.GingerbreadHeartWidgetProvider
 import com.jonasgerdes.stoppelmap.widget.silhouette.SilhouetteWidgetProvider
+
+@Preview
+@Composable
+private fun CountDownWidgetSuggestionCardPreview() {
+    StoppelMapTheme {
+        CountDownWidgetSuggestionCard()
+    }
+}
 
 @Composable
 fun CountDownWidgetSuggestionCard(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     Card {
-        Column(Modifier.padding(16.dp)) {
+        Column {
             Text(
                 text = stringResource(id = R.string.widget_suggestion_card_title),
-                style = MaterialTheme.typography.titleMedium
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(16.dp)
             )
-            WidgetSuggestion(
-                previewDrawable = R.drawable.widget_countdown_preview,
-                previewContentDescription = R.string.widget_countdown_description,
-                onAddTap = { addCountdownWidget(context) },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.size(16.dp))
-            WidgetSuggestion(
-                previewDrawable = R.drawable.widget_preview_heart,
-                previewContentDescription = R.string.widget_suggestion_card_gingerbread_contentDescription,
-                onAddTap = { addGingerbreadWidget(context) },
-                modifier = Modifier.fillMaxWidth()
-            )
-            Spacer(modifier = Modifier.size(16.dp))
-            WidgetSuggestion(
-                previewDrawable = R.drawable.widget_preview_skyline,
-                previewContentDescription = R.string.widget_suggestion_card_skyline_contentDescription,
-                onAddTap = { addSkylineWidget(context) },
-                modifier = Modifier.fillMaxWidth()
-            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier
+                    .horizontalScroll(rememberScrollState())
+                    .height(IntrinsicSize.Max)
+            ) {
+                WidgetSuggestion(
+                    previewDrawable = R.drawable.widget_countdown_preview,
+                    previewContentDescription = R.string.widget_countdown_description,
+                    onAddTap = { addCountdownWidget(context) },
+                    modifier = Modifier.fillMaxHeight()
+                )
+                WidgetSuggestion(
+                    previewDrawable = R.drawable.widget_preview_heart,
+                    previewContentDescription = R.string.widget_suggestion_card_gingerbread_contentDescription,
+                    onAddTap = { addGingerbreadWidget(context) },
+                    modifier = Modifier.fillMaxHeight()
+                )
+                WidgetSuggestion(
+                    previewDrawable = R.drawable.widget_preview_skyline,
+                    previewContentDescription = R.string.widget_suggestion_card_skyline_contentDescription,
+                    onAddTap = { addSkylineWidget(context) },
+                    modifier = Modifier.fillMaxHeight()
+                )
+            }
         }
     }
 }
@@ -76,23 +102,32 @@ fun WidgetSuggestion(
     onAddTap: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Card(shape = RoundedCornerShape(8.dp),
-        modifier = modifier
-            .clickable {
-                onAddTap()
-            }) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        onClick = { onAddTap() },
+        modifier = modifier.padding(8.dp)
+    ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
+                .fillMaxHeight()
+                .padding(8.dp)
         ) {
             Image(
                 painterResource(id = previewDrawable),
                 contentDescription = stringResource(id = previewContentDescription)
             )
             Spacer(modifier = Modifier.size(8.dp))
-            Text(text = stringResource(id = R.string.widget_suggestion_card_add))
+            Button(onClick = { onAddTap() }) {
+                Icon(
+                    Icons.Rounded.Add,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text(text = stringResource(id = R.string.widget_suggestion_card_add))
+            }
         }
     }
 }
