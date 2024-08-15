13a08c92fd53fede45b8634ded972065d8cbf14c
Jonas Gerdes <dev@jonasgerdes.com>
2024-08-15 16:01:32 +0200

    Shared: Fix typo on panama card

--- android/app/src/main/kotlin/com/jonasgerdes/stoppelmap/home/ui/HomeScreen.kt
+++ android/app/src/main/kotlin/com/jonasgerdes/stoppelmap/home/ui/HomeScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -184,7 +185,7 @@ fun HomeScreen(
                                 style = MaterialTheme.typography.titleMedium
                             )
                             Spacer(modifier = Modifier.size(16.dp))
-                            Button(
+                            OutlinedButton(
                                 onClick = { onUrlTap(panamaState.url) },
                                 modifier = Modifier.align(Alignment.CenterHorizontally)
                             ) {
