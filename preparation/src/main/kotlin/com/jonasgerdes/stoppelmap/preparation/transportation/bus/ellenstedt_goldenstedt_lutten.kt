0d05a1cd2fcb83f5da8230d2ff6a2f35b73d3e84
Jonas Gerdes <dev@jonasgerdes.com>
2025-08-08 19:55:26 +0200

    Add bus routes from Friedt/VVN, fetch latest event and tent data

--- preparation/src/main/kotlin/com/jonasgerdes/stoppelmap/preparation/transportation/bus/ellenstedt_goldenstedt_lutten.kt
+++ preparation/src/main/kotlin/com/jonasgerdes/stoppelmap/preparation/transportation/bus/ellenstedt_goldenstedt_lutten.kt
@@ -40,6 +40,13 @@ internal fun ellenstedtGoldenstedtLutten() = createBusRoute {
     }
     addStation("Goldenstedt, Heide", minutesAfterPrevious = 7) {
         prices(400, 200, 6 to 11)
+        outward {
+            thursday("18:07", "19:07", "20:07", "21:07")
+            friday("16:07", "18:07", "19:07", "20:07", "21:07", "22:07")
+            saturday("19:07", "20:07", "21:07", "22:07")
+            monday("09:07", "10:07", "11:07", "12:07", "13:07", "15:07", "17:07", "19:37", "20:37")
+            tuesday("15:07", "16:07", "18:07")
+        }
     }
 
     addStation("Goldenstedt, Heider-Schule", minutesAfterPrevious = 2) {
@@ -68,7 +75,7 @@ internal fun ellenstedtGoldenstedtLutten() = createBusRoute {
         prices(400, 200, 6 to 11)
         outward {
             friday("20:08", "21:08", "22:08")
-            saturday("15:05", "20:08", "21:08", "22:05")
+            saturday("15:05", "20:08", "21:08", "22:05", "22:08")
             monday("09:08", "10:08", "11:08")
         }
         ellenstedtReturns()
@@ -93,7 +100,7 @@ internal fun ellenstedtGoldenstedtLutten() = createBusRoute {
                 "17:11",
                 "20:41",
             )
-            tuesday("15:15", "16:15", "18:15")
+            tuesday("15:11", "16:11", "18:11")
         }
     }
     addStation("Lutten, Kirche") {
@@ -103,7 +110,6 @@ internal fun ellenstedtGoldenstedtLutten() = createBusRoute {
                 "18:15",
                 "19:15",
                 "20:15",
-                "20:20",
                 "21:15",
             )
             friday(
@@ -129,6 +135,20 @@ internal fun ellenstedtGoldenstedtLutten() = createBusRoute {
                 "22:15",
                 "22:20",
             )
+            monday(
+                "09:15",
+                "09:20",
+                "10:15",
+                "10:20",
+                "11:15",
+                "11:20",
+                "12:15",
+                "13:15",
+                "15:15",
+                "17:15",
+                "20:45",
+            )
+            tuesday("15:15", "16:15", "18:15")
         }
     }
     addStation("Lutten, Amerbusch", minutesAfterPrevious = 2) {
@@ -137,6 +157,35 @@ internal fun ellenstedtGoldenstedtLutten() = createBusRoute {
     addStation("Vechta, Oythe", minutesAfterPrevious = 5) {
         prices(300, 150, 6 to 11)
         outward {
+            thursday(
+                "18:22",
+                "19:22",
+                "20:22",
+                "21:22",
+            )
+            friday(
+                "16:22",
+                "18:22",
+                "19:22",
+                "20:22",
+                "20:27",
+                "21:22",
+                "21:27",
+                "22:12",
+                "22:22",
+                "22:27",
+            )
+            saturday(
+                "15:27",
+                "19:22",
+                "20:22",
+                "20:27",
+                "21:22",
+                "21:27",
+                "22:12",
+                "22:22",
+                "22:27",
+            )
             monday(
                 "09:22",
                 "09:27",
@@ -151,6 +200,7 @@ internal fun ellenstedtGoldenstedtLutten() = createBusRoute {
                 "19:40",
                 "20:52",
             )
+            tuesday("15:22", "16:22", "18:22")
         }
     }
 }
