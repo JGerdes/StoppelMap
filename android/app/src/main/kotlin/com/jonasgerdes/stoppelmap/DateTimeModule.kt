5d7119e450b3c2b3f14ddc24bb468f1a0e8418be
Jonas Gerdes <dev@jonasgerdes.com>
2026-06-08 20:22:34 +0200

    Revert "Android: Fix debug date override only works when real date is during season of app"
    This reverts commit 1ff587fe9e120e3d22f924dea27543586a06b3ed.

--- android/app/src/main/kotlin/com/jonasgerdes/stoppelmap/DateTimeModule.kt
+++ android/app/src/main/kotlin/com/jonasgerdes/stoppelmap/DateTimeModule.kt
@@ -2,7 +2,6 @@ package com.jonasgerdes.stoppelmap
 
 import com.jonasgerdes.stoppelmap.base.contract.ClockProvider
 import com.jonasgerdes.stoppelmap.base.contract.SeasonProvider
-import com.jonasgerdes.stoppelmap.base.model.AppInfo
 import com.jonasgerdes.stoppelmap.base.model.VenueInformation
 import com.jonasgerdes.stoppelmap.provider.StoppelmarktClockProvider
 import com.jonasgerdes.stoppelmap.provider.StoppelmarktSeasonProvider
@@ -10,7 +9,6 @@ import com.jonasgerdes.stoppelmap.settings.data.DateOverride
 import com.jonasgerdes.stoppelmap.settings.data.SettingsRepository
 import com.jonasgerdes.stoppelmap.util.clockprovider.ClockProviderWrapper
 import com.jonasgerdes.stoppelmap.util.clockprovider.ExactDayClockProvider
-import com.jonasgerdes.stoppelmap.util.clockprovider.OverrideYearClockProvider
 import com.jonasgerdes.stoppelmap.util.clockprovider.TodayInStoMaWeekClockProvider
 import kotlinx.datetime.DayOfWeek
 import org.koin.dsl.module
@@ -18,11 +16,8 @@ import org.koin.dsl.module
 val dateTimeModule = module {
     single<SeasonProvider> { StoppelmarktSeasonProvider(clockProvider = get()) }
     single<ClockProvider> {
-        val realClockProvider = OverrideYearClockProvider(
-            realClockProvider = StoppelmarktClockProvider(
-                localTimeZone = get<VenueInformation>().timeZone,
-            ),
-            year = get<AppInfo>().versionCode.toString().take(2).toInt() + 2000
+        val realClockProvider = StoppelmarktClockProvider(
+            localTimeZone = get<VenueInformation>().timeZone
         )
         val realSeasonProvider = StoppelmarktSeasonProvider(clockProvider = realClockProvider)
         val localTimeZone = get<VenueInformation>().timeZone
